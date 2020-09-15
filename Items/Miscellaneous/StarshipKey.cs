9d6f0d9155093e5d4f31189f54f5c75f5845f388
MechanicPluto24 <32751326+MechanicPluto24@users.noreply.github.com>
2020-09-14 23:27:53 -0400

    Protolith (Moon Rock)

--- Items/Miscellaneous/StarshipKey.cs
+++ Items/Miscellaneous/StarshipKey.cs
@@ -2,7 +2,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Macrocosm.Items.Miscellanious
+namespace Macrocosm.Items.Miscellaneous
 {
     public class StarshipKey : ModItem
     {
