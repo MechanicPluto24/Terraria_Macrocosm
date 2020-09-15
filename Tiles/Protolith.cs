aed16f732b07a49eaf8a66cd72d2f92875105c07
MechanicPluto24 <32751326+MechanicPluto24@users.noreply.github.com>
2020-09-14 23:27:53 -0400

    Protolith (Moon Rock)

---
+++ Tiles/Protolith.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Macrocosm.Tiles
+{
+	public class Protolith : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileLighted[Type] = true;
+			Main.tileMergeDirt[Type] = true;
+			minPick = 225;
+			mineResist = 3f;
+			drop = ItemType<Items.Placeables.BlocksAndWalls.Protolith>();
+			AddMapEntry(new Color(65, 65, 65));
+			soundType = SoundID.Tink;
+		}
+	}
+}
