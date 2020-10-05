b0152bc102a615b45304263488474206eb2def7c
RyanMakesMods <60587654+RyanMakesMods@users.noreply.github.com>
2020-10-04 19:14:13 -0500

    balanced the astronaut suit

--- Items/Armor/AstronautLeggings.cs
+++ Items/Armor/AstronautLeggings.cs
@@ -17,7 +17,7 @@ namespace Macrocosm.Items.Armor
 			item.height = 18;
 			item.value = 10000;
 			item.rare = ItemRarityID.Green;
-			item.defense = 10;
+			item.defense = = 22;
 		}
 
 		public override void UpdateEquip(Player player) {
