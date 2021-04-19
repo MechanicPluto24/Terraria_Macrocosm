5ce12be2fd5b2b21cb1edc3d133bb07ba3f116aa
MechanicPluto24 <32751326+MechanicPluto24@users.noreply.github.com>
2021-04-19 12:19:00 -0400

    More paintings!!
    Blood Sun Rises, Empress, Overlord, Retributor, and When Day Breaks

---
+++ Items/Placeables/Paintings/BloodSunRising.cs
@@ -0,0 +1,37 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Macrocosm.Items.Placeables.Paintings
+{
+    public class BloodSunRising : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Blood Sun Rising");
+            Tooltip.SetDefault("'L. Reda'");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 32;
+            item.height = 22;
+            item.maxStack = 999;
+            item.value = 0;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.consumable = true;
+            item.createTile = TileType<Tiles.Paintings.BloodSunRising>();
+            item.placeStyle = 0;
+        }
+
+        public override void AddRecipes()
+        {
+
+        }
+    }
+}
