f62cbb23751908f5d82b0f39aba6c3366e67bb31
RyanMakesMods <60587654+RighteousRyan1@users.noreply.github.com>
2021-07-11 22:11:52 -0500

    No More TerrariaAmbienceAPI Dependency.

--- Common/MacrocosmUtils.cs
+++ Common/MacrocosmUtils.cs
@@ -4,6 +4,7 @@ using Macrocosm.Content.NPCs.Unfriendly.Enemies;
 using Terraria;
 using System;
 using System.Reflection;
+using Microsoft.Xna.Framework;
 
 namespace Macrocosm.Common
 {
@@ -149,4 +150,19 @@ namespace Macrocosm.Common
             return (T)field.GetValue(obj);
         }
     }
+	public static class ColorManipulator 
+	{
+		public static void ManipulateColor(ref Color color, byte amount)
+		{
+			color.R += amount;
+			color.G += amount;
+			color.B += amount;
+		}
+		public static void ManipulateColor(ref Color color, float amount)
+		{
+			color.R *= (byte)Math.Round(color.R * amount);
+			color.G += (byte)Math.Round(color.G * amount);
+			color.B += (byte)Math.Round(color.B * amount);
+		}
+	}
 }
