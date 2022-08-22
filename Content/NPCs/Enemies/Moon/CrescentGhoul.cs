2593583ef421c1807c8fee1443d6a208484a795e
GeorgeFeldy <georgefeldy@gmail.com>
2022-08-22 04:50:03 +0300

    Better Ghoul AI

--- Content/NPCs/Enemies/Moon/CrescentGhoul.cs
+++ Content/NPCs/Enemies/Moon/CrescentGhoul.cs
@@ -6,6 +6,7 @@ using Macrocosm.Content.Dusts;
 using Macrocosm.Content.Items.Materials;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria;
 using Terraria.GameContent;
 using Terraria.GameContent.Bestiary;
@@ -20,7 +21,9 @@ namespace Macrocosm.Content.NPCs.Enemies.Moon
 		public enum ActionState
 		{
 			Chase,
-			Dash
+			StartSpin,
+			Spin,
+			StopSpin
 		}
 
 		public ActionState AI_State
@@ -28,6 +31,7 @@ namespace Macrocosm.Content.NPCs.Enemies.Moon
 			get => (ActionState)NPC.ai[0];
 			set => NPC.ai[0] = (int)value;
 		}
+
 		public ref float AI_Timer => ref NPC.ai[1];
 
 		public override void SetStaticDefaults()
@@ -46,11 +50,12 @@ namespace Macrocosm.Content.NPCs.Enemies.Moon
 
 			NPC.width = 72;
 			NPC.height = 84;
-			NPC.lifeMax = 2500;
-			NPC.damage = 60;
+			NPC.lifeMax = 4000;
+			NPC.damage = 200;
 			NPC.defense = 60;
 			NPC.HitSound = SoundID.NPCHit2;
 			NPC.DeathSound = SoundID.NPCDeath2;
+			NPC.aiStyle = -1;
 			NPC.value = 60f;
 			NPC.knockBackResist = 0f;
 			NPC.noGravity = true;
@@ -67,44 +72,146 @@ namespace Macrocosm.Content.NPCs.Enemies.Moon
 			});
 		}
 
+		/// <summary>
+		/// Adapted from Corite AI 
+		/// </summary>
 		public override void AI()
 		{
-			Player player = Main.player[NPC.target];
-
 			if (NPC.target < 0 || NPC.target == 255 || Main.player[NPC.target].dead || !Main.player[NPC.target].active)
-			{
 				NPC.TargetClosest(true);
-			}
 
-			AI_Timer++;
+			bool playerActive = Main.player[NPC.target] != null && Main.player[NPC.target].active && !Main.player[NPC.target].dead;
 
-			switch (AI_State)
+
+			float kbResist = 0.3f;
+
+			float chaseUpwardsMult = 8f;
+
+			// the radius where the ghoul will slow down and start rising above the player 
+			float chaseRadius = 60f; 
+
+			float startSpinDeceleration = 0.8f;
+			float startSpinDuration = 5f;
+
+			float maxDashDistance = 400f;
+			float dashAngleDiv = 8f;
+			float dashSpeed = 10f; 
+
+			int dashAngleVariation = 0; // some angle variation for the dash (?)
+
+			// breaks the dash if:
+			//float dashChaseDuration = 10f; // this many ticks passed 
+			//float dashStopDistance = 150f; // distance is greater than this 
+
+			float dashStopSpeed = 8f; // also breaks the dash if the length of velocity vector is below this value 
+
+			// the radius around the player where the ghoul tends to spin 
+			float dashRadius = 60f;
+			// huh 
+			float dashFactorThird = 0.33333334f * dashRadius;
+
+			if (Main.expertMode)
+				kbResist *= Main.GameModeInfo.KnockbackToEnemiesMultiplier;
+
+			if(AI_State != ActionState.Spin)
+				BaseAI.LookAt(playerActive ? Main.player[NPC.target].Center : NPC.Center + NPC.velocity, NPC, 0);
+
+			if (AI_State == ActionState.Chase)
 			{
-				case ActionState.Chase:
-					NPC.Move(player.Center, Vector2.Zero);
-					bool playerActive = player != null && player.active && !player.dead;
-					BaseAI.LookAt(playerActive ? player.Center : NPC.Center + NPC.velocity, NPC, 0);
+				NPC.knockBackResist = kbResist;
 
-					if (AI_Timer > 200 && Vector2.Distance(NPC.Center, player.Center) < 300f)
+				Vector2 positionDiff = Main.player[NPC.target].Center - NPC.Center ;
+				Vector2 upwards = positionDiff - Vector2.UnitY * 300f;
+				float distanceToTarget = positionDiff.Length();
+				positionDiff = Vector2.Normalize(positionDiff) * chaseUpwardsMult;
+				upwards = Vector2.Normalize(upwards) * chaseUpwardsMult;
+				bool canDash = Collision.CanHit(NPC.Center, 1, 1, Main.player[NPC.target].Center, 1, 1);
+
+				if (NPC.ai[3] >= 120f)
+					canDash = true;
+
+				canDash = (canDash && positionDiff.ToRotation() > (float)Math.PI / dashAngleDiv && positionDiff.ToRotation() < (float)Math.PI - (float)Math.PI / dashAngleDiv);
+				
+				if (distanceToTarget > maxDashDistance || !canDash)
+				{
+					NPC.velocity.X = (NPC.velocity.X * (chaseRadius - 1f) + upwards.X) / chaseRadius;
+					NPC.velocity.Y = (NPC.velocity.Y * (chaseRadius - 1f) + upwards.Y) / chaseRadius;
+					if (!canDash)
 					{
-						AI_Timer = 0;
-						AI_State = ActionState.Dash;
+						NPC.ai[3]++;
+						if (NPC.ai[3] == 120f)
+							NPC.netUpdate = true;
 					}
-					break;
-
-				case ActionState.Dash:
-
-					NPC.rotation += 0.36f;
-
-					NPC.Move(player.Center, Vector2.Zero, 7f);
-
-					if (AI_Timer > 100 || Vector2.Distance(NPC.Center, player.Center) < 30f)
+					else
 					{
-						AI_Timer = 0;
-						AI_State = ActionState.Chase;
+						NPC.ai[3] = 0f;
 					}
+				}
+				else
+				{
+					AI_State = ActionState.StartSpin;
+					NPC.ai[2] = positionDiff.X;
+					NPC.ai[3] = positionDiff.Y;
+					NPC.netUpdate = true;
+				}
+			}
+			else if (AI_State == ActionState.StartSpin)
+			{
+				NPC.knockBackResist = 0f;
+				NPC.velocity *= startSpinDeceleration;
+				AI_Timer++;
+				if (AI_Timer >= startSpinDuration)
+				{
+					AI_State = ActionState.Spin;
+					AI_Timer = 0f;
+					NPC.netUpdate = true;
+					Vector2 dashVector = new Vector2(NPC.ai[2], NPC.ai[3]) + new Vector2(Main.rand.Next(-dashAngleVariation, dashAngleVariation + 1), Main.rand.Next(-dashAngleVariation, dashAngleVariation + 1)) * 0.04f;
+					dashVector.Normalize();
+					NPC.velocity = dashVector * dashSpeed;
+				}
 
-					break;
+			}
+			else if (AI_State == ActionState.Spin)
+			{
+
+				NPC.rotation += 0.48f;
+				NPC.knockBackResist = 0f;
+ 				AI_Timer++;
+
+				//bool playerClose = Vector2.Distance(NPC.Center, Main.player[NPC.target].Center) > dashStopDistance; /* corites also check for player below : && NPC.Center.Y > Main.player[NPC.target].Center.Y;*/
+				//if ((AI_Timer >= dashChaseDuration && playerClose) || NPC.velocity.Length() < dashStopSpeed)
+
+				if (NPC.velocity.Length() < dashStopSpeed)
+				{
+					AI_State = ActionState.StopSpin;
+					AI_Timer = 45f;
+					NPC.ai[2] = 0f;
+					NPC.ai[3] = 0f;
+					NPC.velocity /= 2f;
+					NPC.netUpdate = true;
+				}
+				else
+				{
+					Vector2 positionDiff = Main.player[NPC.target].Center - NPC.Center;
+					positionDiff.Normalize();
+					if (positionDiff.HasNaNs())
+						positionDiff = new Vector2(NPC.direction, 0f);
+
+					NPC.velocity = (NPC.velocity * (dashRadius - 1f) + positionDiff * (NPC.velocity.Length() + dashFactorThird)) / dashRadius;
+				}
+
+			}
+			else if (AI_State == ActionState.StopSpin)
+			{
+				AI_Timer -= 3f;
+				if (AI_Timer <= 0f)
+				{
+					AI_State = ActionState.Chase;
+					AI_Timer = 0f;
+					NPC.netUpdate = true;
+				}
+
+				NPC.velocity *= 0.95f;
 			}
 		}
 
@@ -131,14 +238,14 @@ namespace Macrocosm.Content.NPCs.Enemies.Moon
 
 		public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
 		{
-			if (AI_State != ActionState.Dash)
+			if (AI_State != ActionState.Spin)
 				return true;
 
 			for (int i = 0; i < NPC.oldPos.Length; i++)
 			{
 				Vector2 drawPos = NPC.oldPos[i] + NPC.Size / 2 - Main.screenPosition;
 				Color color = NPC.GetAlpha(drawColor) * (((float)NPC.oldPos.Length - i) / NPC.oldPos.Length);
-				spriteBatch.Draw(TextureAssets.Npc[NPC.type].Value, drawPos, NPC.frame, color * 0.6f, NPC.rotation - 0.36f * i, NPC.Size / 2, NPC.scale, SpriteEffects.None, 0f);
+				spriteBatch.Draw(TextureAssets.Npc[NPC.type].Value, drawPos, NPC.frame, color * 0.6f, NPC.rotation - 0.48f * i, NPC.Size / 2, NPC.scale, SpriteEffects.None, 0f);
 			}
 
 			return true;
