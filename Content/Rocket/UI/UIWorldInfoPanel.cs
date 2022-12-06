edfe3da6c85711da072c73dfbad79d463b575c0a
GeorgeFeldy <georgefeldy@gmail.com>
2022-12-06 13:13:19 +0200

    cleanup

--- Content/Rocket/UI/UIWorldInfoPanel.cs
+++ Content/Rocket/UI/UIWorldInfoPanel.cs
@@ -3,9 +3,6 @@
 using Macrocosm.Content.Subworlds;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Collections.Generic;
-using Terraria;
 using Terraria.GameContent.UI.Elements;
 using Terraria.ModLoader;
 using Terraria.UI;
@@ -15,13 +12,9 @@ namespace Macrocosm.Content.Rocket.UI
 	public class UIWorldInfoPanel : UIPanel
 	{
 		public string Name = "";
-		public string Text = "";
 
 		private UIText UIDisplayName;
-		private UIText UIDisplayText;
-
-		private UIList InfoElements;
-		private UIScrollbar Scrollbar;
+		private UIList UIInfoElements;
 		
 		public override void OnInitialize()
 		{
@@ -38,25 +31,28 @@ namespace Macrocosm.Content.Rocket.UI
 				Width = StyleDimension.FromPixelsAndPercent(0f, 1f),
 				Height = StyleDimension.FromPixelsAndPercent(0f, 1f)
 			};
+
 			uIList.SetPadding(2f);
 			uIList.PaddingBottom = 4f;
 			uIList.PaddingTop = 50f;
 			uIList.PaddingLeft = 6f;
 			Append(uIList);
-			InfoElements = uIList;
+			UIInfoElements = uIList;
 
 			uIList.ListPadding = 4f;
 			uIList.ManualSortMethod = (_) => { };
+
 			UIScrollbar uIScrollbar = new UIScrollbar();
 			uIScrollbar.SetView(150f, 1000f);
 			uIScrollbar.Height.Set(0f, 0.95f);
 			uIScrollbar.HAlign = 0.99f;
 			uIScrollbar.VAlign = 0.5f;
- 			//this._scrollbar = uIScrollbar;
-			InfoElements.SetScrollbar(uIScrollbar);
+			UIInfoElements.SetScrollbar(uIScrollbar);
+
 			Append(uIScrollbar);
-			InfoElements.Width.Set(-20f, 1f);
-  			UIDisplayName = new(Text, 1.5f, false)
+			UIInfoElements.Width.Set(-20f, 1f);
+
+  			UIDisplayName = new(Name, 1.5f, false)
 			{
 				HAlign = 0.43f,
 				Top = new StyleDimension(15, 0f),
@@ -64,19 +60,11 @@ namespace Macrocosm.Content.Rocket.UI
 			};
 			Append(UIDisplayName);
 
-			//UIDisplayText = new(Text, 1.1f, false)
-			//{
-			//	TextColor = Color.White
-			//};
-			//UIDisplayText.Left.Set(6f, 0f);
-			//UIDisplayText.Top.Set(40, 0f);
-			//
-			////panel.Append(UIDisplayText);
 		}
 
 		public void FillWithInfo(SubworldData data)
 		{
-			InfoElements.Clear();
+			UIInfoElements.Clear();
 
 			string iconPath = "Macrocosm/Content/Rocket/UI/Icons/";
 
@@ -88,37 +76,36 @@ namespace Macrocosm.Content.Rocket.UI
 
 			string gravData = data.SpecialGravity != "" ? data.SpecialGravity : data.Gravity > 0 ? data.Gravity.ToString() + " G" : "";
 			if (gravData != "")
-				InfoElements.Add(new UIWorldInfoElement(ModContent.Request<Texture2D>(iconPath + "Gravity").Value, gravData, "Gravity"));
+				UIInfoElements.Add(new UIWorldInfoElement(ModContent.Request<Texture2D>(iconPath + "Gravity").Value, gravData, "Gravity"));
 
 			string radiusData = data.SpecialRadius != "" ? data.SpecialRadius : data.Radius > 0 ? data.Radius.ToString() + " km": "";
 			if (radiusData != "")
-				InfoElements.Add(new UIWorldInfoElement(ModContent.Request<Texture2D>(iconPath + "Radius").Value, radiusData, "Radius"));
+				UIInfoElements.Add(new UIWorldInfoElement(ModContent.Request<Texture2D>(iconPath + "Radius").Value, radiusData, "Radius"));
 
 			string dayLenghtData = data.SpecialDayPeriod != "" ? data.SpecialDayPeriod : data.DayPeriod > 0 ? data.DayPeriod.ToString() + " days" : "";
 			if (dayLenghtData != "")
-				InfoElements.Add(new UIWorldInfoElement(ModContent.Request<Texture2D>(iconPath + "DayPeriod").Value, dayLenghtData, "Day period"));
+				UIInfoElements.Add(new UIWorldInfoElement(ModContent.Request<Texture2D>(iconPath + "DayPeriod").Value, dayLenghtData, "Day period"));
 
 			string threatData = data.ThreatLevel > 0 ? data.ThreatLevel.ToString() : "";
 			if (threatData != "")
-				InfoElements.Add(new UIWorldInfoElement(ModContent.Request<Texture2D>(iconPath + "ThreatLevel").Value, threatData, "Threat Level"));
+				UIInfoElements.Add(new UIWorldInfoElement(ModContent.Request<Texture2D>(iconPath + "ThreatLevel").Value, threatData, "Threat Level"));
 
 			if(data.Hazards.Count > 0)
 			{
-				InfoElements.Add(uIHorizontalSeparator);
+				UIInfoElements.Add(uIHorizontalSeparator);
 
 				foreach (var hazard in data.Hazards)
 				{
-					InfoElements.Add(new UIWorldInfoElement(ModContent.Request<Texture2D>(iconPath + hazard.Key).Value, hazard.Value, "Hazard"));
+					UIInfoElements.Add(new UIWorldInfoElement(ModContent.Request<Texture2D>(iconPath + hazard.Key).Value, hazard.Value, "Hazard"));
 				}
 			}
 			
- 			InfoElements.Recalculate();
+ 			UIInfoElements.Recalculate();
 		}
 
 		public override void Update(GameTime gameTime)
 		{
 			base.Update(gameTime);
-			//UIDisplayText.SetText(Text);
 			UIDisplayName.SetText(Name);
 		}
 	}
