27619f8d4e6ed3279c1ebb0f6c3f66407c3f75e1
George Feldy <45595425+GeorgeFeldy@users.noreply.github.com>
2023-05-23 02:52:45 +0300

    Added more updated stuff

--- Content/Projectiles/Friendly/Magic/ImbriumJewelMeteor.cs
+++ Content/Projectiles/Friendly/Magic/ImbriumJewelMeteor.cs
@@ -1,3 +1,4 @@
+using Macrocosm.Common.Utils;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
@@ -9,8 +10,6 @@ namespace Macrocosm.Content.Projectiles.Friendly.Magic
 {
     public class ImbriumJewelMeteor : ModProjectile
     {
-		public override string Texture => Mod.Name + "/Content/Projectiles/Hostile/FlamingMeteor";
-
 		public override void SetStaticDefaults()
 		{
             Main.projFrames[Type] = 6;
@@ -23,26 +22,24 @@ namespace Macrocosm.Content.Projectiles.Friendly.Magic
             Projectile.friendly = true;
             Projectile.timeLeft = 120;
             Projectile.DamageType = DamageClass.Magic;
+            Projectile.alpha = 0;
         }
 
         public override void AI()
         {
-            Projectile.velocity.Y += 0.25f;
-            if (Projectile.velocity.Y > 16f)
-                Projectile.velocity.Y = 16f;
+            Projectile.velocity.Y += 0.1f;
+            if (Projectile.velocity.Y > 12f)
+                Projectile.velocity.Y = 12f;
 
             if (Projectile.velocity != Vector2.Zero)
                 Projectile.rotation = Projectile.velocity.ToRotation() - MathHelper.PiOver2;
 
-            if (Main.rand.NextFloat() < 0.35f)
+            if (Main.rand.NextFloat() < 0.8f)
             {
-                int num5 = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Torch, Projectile.velocity.X * 0.4f, Projectile.velocity.Y * 0.4f, 128, default, 3f);
-                Main.dust[num5].noGravity = true;
-                Main.dust[num5].velocity.X *= 2f;
-                Main.dust[num5].velocity.Y *= 1.5f;
-
-                if (num5 % 2 == 0)
-                    Main.dust[num5].color = new Color(0, 255, 100);
+                int dustIdx = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GreenTorch, Projectile.velocity.X * 0.4f, Projectile.velocity.Y * 0.4f, 128, new Color(0, 255, 100), 1.8f);
+                Main.dust[dustIdx].noGravity = true;
+                Main.dust[dustIdx].velocity.X *= 2f;
+                Main.dust[dustIdx].velocity.Y *= 1.5f;
             }
 
 
@@ -51,6 +48,9 @@ namespace Macrocosm.Content.Projectiles.Friendly.Magic
                 Projectile.frameCounter = 0;
                 Projectile.frame = ++Projectile.frame % Main.projFrames[Type]; // 6 frames @ 4 ticks/frame
             }
+
+            if (Projectile.alpha < 250)
+                Projectile.alpha += 10;
         }
 
         public override bool PreDraw(ref Color lightColor)
@@ -59,11 +59,14 @@ namespace Macrocosm.Content.Projectiles.Friendly.Magic
 
             Rectangle sourceRect = tex.Frame(1, Main.projFrames[Type], frameY: Projectile.frame);
 
-            Vector2 origin = Projectile.Size / 2f + new Vector2(6, 32);
+            Vector2 origin = Projectile.Size / 2f + new Vector2(0, 16);
 
+            SpriteBatchState state = Main.spriteBatch.SaveState();
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(BlendState.Additive, state);
             Main.EntitySpriteDraw(TextureAssets.Projectile[Type].Value, Projectile.Center - Main.screenPosition,
-                sourceRect, Color.White, Projectile.rotation, origin, Projectile.scale, SpriteEffects.None, 0);
-
+                sourceRect, new Color(255, 255, 255, Projectile.alpha), Projectile.rotation, origin, Projectile.scale, SpriteEffects.None, 0);
+            Main.spriteBatch.Restore(state);
             return false;
         }
 
@@ -76,11 +79,12 @@ namespace Macrocosm.Content.Projectiles.Friendly.Magic
             SoundEngine.PlaySound(SoundID.Item89, Projectile.position);
             for (int i = 0; i < 10; i++)
             {
-                int num5 = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Torch, Projectile.velocity.X * 0.4f, Projectile.velocity.Y * 0.4f, 128, default, 3f);
-                Main.dust[num5].noGravity = true;
-                Main.dust[num5].velocity.X *= 2f;
-                Main.dust[num5].velocity.Y *= 1.5f;
-            }
-        }
+                int dustIdx = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.GreenTorch, Projectile.velocity.X * 0.4f, Projectile.velocity.Y * 0.4f, 128, new Color(255, 255, 255), 3f);
+                Main.dust[dustIdx].noGravity = true;
+                Main.dust[dustIdx].velocity.X *= 2f;
+                Main.dust[dustIdx].velocity.Y *= 1.5f;
+
+			}
+		}
     }
 }
