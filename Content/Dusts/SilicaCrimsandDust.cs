59cec96b66509973a7d299a008b234662523c04d
George Feldy <45595425+GeorgeFeldy@users.noreply.github.com>
2023-06-12 04:41:07 +0300

    added sands. to fix tileblend add clentaminator and sandgun (?) support

---
+++ Content/Dusts/SilicaCrimsandDust.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace Macrocosm.Content.Dusts
+{
+	public class SilicaCrimsandDust : ModDust
+	{
+		public override bool Update(Dust dust)
+		{
+			dust.scale -= 0.01f;
+			dust.velocity.X *= 0.96f;
+
+			if (!dust.noGravity)
+			{
+				dust.velocity.Y += 0.1f;
+			}
+
+			return true;
+		}
+	}
+}
