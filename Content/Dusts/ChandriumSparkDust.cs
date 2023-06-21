f7c8214a6334aae9fbf3dc348afa451e1a36fb4d
George Feldy <45595425+GeorgeFeldy@users.noreply.github.com>
2023-06-22 01:46:33 +0300

    Removed custom dust draw IL hook, replaced with TML hook which works practically the same.

--- Content/Dusts/ChandriumSparkDust.cs
+++ Content/Dusts/ChandriumSparkDust.cs
@@ -1,4 +1,3 @@
-using Macrocosm.Common.Drawing.Dusts;
 using Macrocosm.Common.Utils;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -9,7 +8,7 @@ using Terraria.ModLoader;
 
 namespace Macrocosm.Content.Dusts
 {
-	public class ChandriumSparkDust : ModDust, IDustCustomDraw
+	public class ChandriumSparkDust : ModDust
 	{
 		public override bool Update(Dust dust)
 		{
@@ -41,10 +40,9 @@ namespace Macrocosm.Content.Dusts
 		public override Color? GetAlpha(Dust dust, Color lightColor)
 			=> Color.White.NewAlpha(0.5f);
 
-		public bool DrawDust(Dust dust,SpriteBatch spriteBatch, Vector2 screenPosition, Texture2D texture, Rectangle dustFrame)
+		public override bool PreDraw(Dust dust)
 		{
 			float count = Math.Abs(dust.velocity.X) + Math.Abs(dust.velocity.Y) * 3f;
-
 			if (count > 10f)
 				count = 10f;
 
@@ -53,7 +51,7 @@ namespace Macrocosm.Content.Dusts
 				Vector2 trailPosition = dust.position - dust.velocity * n;
 				float scale = dust.scale * (1f - n / 10f);
 				Color color = Lighting.GetColor((int)(dust.position.X + 4.0) / 16, (int)(dust.position.Y + 4.0) / 16);
-				spriteBatch.Draw(texture, trailPosition - Main.screenPosition, dustFrame, dust.GetAlpha(color), dust.rotation, new Vector2(4f, 4f), scale, SpriteEffects.None, 0f);
+				Main.spriteBatch.Draw(dust.GetTexture(), trailPosition - Main.screenPosition, dust.frame, dust.GetAlpha(color), dust.rotation, new Vector2(4f, 4f), scale, SpriteEffects.None, 0f);
 			}
 
 			return true;
