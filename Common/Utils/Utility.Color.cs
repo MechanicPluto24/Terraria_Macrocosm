1145b1153d4a0327e2ec519d5c5185b7939812e5
George Feldy <45595425+GeorgeFeldy@users.noreply.github.com>
2023-09-10 00:44:13 +0300

    lunar plating update

--- Common/Utils/Utility.Color.cs
+++ Common/Utils/Utility.Color.cs
@@ -7,8 +7,16 @@ namespace Macrocosm.Common.Utils
 {
 	public static partial class Utility
     {
-		public static string GetHexText(this Color color) => "#" + color.Hex3().ToUpper();
+		#region Alpha manipulation
+		public static Color WithOpacity(this Color color, float opacity)
+			=> new(color.R, color.G, color.B, (byte)(opacity * 255));
 
+		public static Color WithAlpha(this Color color, byte alpha)
+			=> new(color.R, color.G, color.B, alpha);
+		#endregion
+
+		#region Hex format conversion
+		public static string GetHexText(this Color color) => "#" + color.Hex3().ToUpper();
 		public static bool TryGetColorFromHex(string hexString, out Color color)
 		{
 			if (hexString.StartsWith("#"))
@@ -26,7 +34,6 @@ namespace Macrocosm.Common.Utils
 			color = Color.White;
 			return false;
 		}
-
 		public static bool TryGetColorFromHex(string hexString, out Vector3 hsl, float luminanceFactor = 1f)
 		{
 			if (TryGetColorFromHex(hexString, out Color rgb))
@@ -40,8 +47,9 @@ namespace Macrocosm.Common.Utils
 				return false;
 			}
 		}
+		#endregion
 
-
+		#region HSL conversion
 		public static Vector3 RGBToHSL(Color color) => color.ToHSL();
 		public static Vector3 ToHSL(this Color color) => Main.rgbToHsl(color);
 
@@ -71,6 +79,8 @@ namespace Macrocosm.Common.Utils
 
 		public static Vector3 ScaleHSL(Vector3 hsl, float luminanceFactor) => ScaleHSL(hsl.X, hsl.Y, hsl.Z, luminanceFactor);
 
+		#endregion
+
 		public static void ManipulateColor(ref Color color, byte amount)
         {
             color.R += amount;
@@ -84,7 +94,134 @@ namespace Macrocosm.Common.Utils
             color.B += (byte)Math.Round(color.B * amount);
         }
 
-        public static Vector3[] ToVector3Array(this Color[] colors)
+		/// <summary>
+		///	 Returns a monochrome version of the given color.		///	</summary>
+
+		public static Color ColorMonochrome(Color color)
+		{
+			int average = color.R + color.G + color.B;
+			average /= 3;
+			return new Color(average, average, average, color.A);
+		}
+
+		/// <summary> Gets the perceived luminance of a color using the NTSC standard as a normalized value </summary>
+		public static float GetLuminanceNTSC(this Color rgbColor)
+			=> 0.299f * rgbColor.R / 255 + 0.587f * rgbColor.G / 255 + 0.114f * rgbColor.B / 255;
+
+		/// <summary> Returns the RGB grayscale of a color using the NTSC standard </summary>
+		public static Color ToGrayscaleNTSC(this Color rgbColor)
+		{
+			Color result = new();
+			result.R = result.G = result.B = (byte)(rgbColor.GetLuminanceNTSC() * 255);
+			result.A = rgbColor.A;
+			return result;
+		}
+		///<summary>
+		/// Alters the brightness of the color by the amount of the factor. If factor is negative, it darkens it. Else, it brightens it.		///</summary>
+		public static Color Brighten(this Color color, int factor)
+		{
+			int r = Math.Max(0, Math.Min(255, color.R + factor));
+			int g = Math.Max(0, Math.Min(255, color.G + factor));
+			int b = Math.Max(0, Math.Min(255, color.B + factor));
+			return new Color(r, g, b, color.A);
+		}
+
+		///<summary>
+		/// Alters the brightness of the color by the multiplier.		///</summary>
+		public static Color ColorMult(Color color, float mult)
+		{
+			int r = Math.Max(0, Math.Min(255, (int)(color.R * mult)));
+			int g = Math.Max(0, Math.Min(255, (int)(color.G * mult)));
+			int b = Math.Max(0, Math.Min(255, (int)(color.B * mult)));
+			return new Color(r, g, b, color.A);
+		}
+
+		///<summary>
+		/// Clamps the first color to be no lower then the values of the second color.		///</summary>
+		public static Color ColorClamp(Color color1, Color color2)
+		{
+			int r = color1.R;
+			int g = color1.G;
+			int b = color1.B;
+			int a = color1.A;
+			if (r < color2.R) { r = color2.R; }
+			if (g < color2.G) { g = color2.G; }
+			if (b < color2.B) { b = color2.B; }
+			if (a < color2.A) { a = color2.A; }
+			return new Color(r, g, b, a);
+		}
+
+		///<summary>
+		/// Clamps the first color to be no lower then the brightness of the second color.		///</summary>
+		public static Color ColorBrightnessClamp(Color color1, Color color2)
+		{
+			float r = color1.R / 255f;
+			float g = color1.G / 255f;
+			float b = color1.B / 255f;
+			float r2 = color2.R / 255f;
+			float g2 = color2.G / 255f;
+			float b2 = color2.B / 255f;
+			float brightness = r2 > g2 ? r2 : g2 > b2 ? g2 : b2;
+			r *= brightness; g *= brightness; b *= brightness;
+			return new Color(r, g, b, color1.A / 255f);
+		}
+
+		///<summary>
+		/// Tints the light color according to the buff color given. (prevents 'darkness' occuring if more than one is applied)		///</summary>
+		public static Color BuffColorize(Color buffColor, Color lightColor)
+		{
+			Color color2 = ColorBrightnessClamp(buffColor, lightColor);
+			return ColorClamp(Colorize(buffColor, lightColor), color2);
+		}
+
+		///<summary>
+		/// Tints the light color according to the tint color given.		///</summary>
+		public static Color Colorize(Color tint, Color lightColor)
+		{
+			float r = lightColor.R / 255f;
+			float g = lightColor.G / 255f;
+			float b = lightColor.B / 255f;
+			float a = lightColor.A / 255f;
+			Color newColor = tint;
+			float nr = (byte)(newColor.R * r);
+			float ng = (byte)(newColor.G * g);
+			float nb = (byte)(newColor.B * b);
+			float na = (byte)(newColor.A * a);
+			newColor.R = (byte)nr;
+			newColor.G = (byte)ng;
+			newColor.B = (byte)nb;
+			newColor.A = (byte)na;
+			return newColor;
+		}
+
+		///<summary>
+		///Returns a color of the rainbow. Percent goes from 0 to 1.
+		/// </summary>
+		public static Color Rainbow(float percent)
+		{
+			Color r = new(255, 50, 50);
+			Color g = new(50, 255, 50);
+			Color b = new(90, 90, 255);
+			Color y = new(255, 255, 50);
+			if (percent <= 0.25f)
+			{
+				return Color.Lerp(r, b, percent / 0.25f);
+			}
+
+			if (percent <= 0.5f)
+			{
+				return Color.Lerp(b, g, (percent - 0.25f) / 0.25f);
+			}
+
+			if (percent <= 0.75f)
+			{
+				return Color.Lerp(g, y, (percent - 0.5f) / 0.25f);
+			}
+
+			return Color.Lerp(y, r, (percent - 0.75f) / 0.25f);
+		}
+
+		public static Vector3[] ToVector3Array(this Color[] colors)
         {
             Vector3[] vectors = new Vector3[colors.Length];
 
@@ -103,26 +240,5 @@ namespace Macrocosm.Common.Utils
 
 			return vectors;
 		}
-
-
-        /// <summary> Gets the perceived luminance of a color using the NTSC standard as a normalized value </summary>
-        public static float GetLuminance(this Color rgbColor)
-            => 0.299f * rgbColor.R / 255 + 0.587f * rgbColor.G / 255 + 0.114f * rgbColor.B / 255;
-
-        /// <summary> Returns the RGB grayscale of a color using the NTSC standard </summary>
-        public static Color ToGrayscale(this Color rgbColor)
-        {
-            Color result = new();
-            result.R = result.G = result.B = (byte)(rgbColor.GetLuminance() * 255);
-            result.A = rgbColor.A;
-            return result;
-        }
-
-        public static Color WithOpacity(this Color color, float opacity)
-            => new(color.R, color.G, color.B, (byte)(opacity * 255));
-
-        public static Color WithAlpha(this Color color, byte alpha)
-            => new(color.R, color.G, color.B, alpha);
-        
 	}
 }
--- Content/Projectiles/Hostile/LuminiteSlimeProjectile.cs
+++ Content/Projectiles/Hostile/LuminiteSlimeProjectile.cs
@@ -39,18 +39,18 @@ namespace Macrocosm.Content.Projectiles.Hostile
 		private SpriteBatchState state;
 		public override bool PreDraw(ref Color lightColor)
 		{
-			Projectile.DrawMagicPixelTrail(Vector2.Zero, 4f, 1f, new Color(98, 211, 168, 255) * lightColor.GetLuminance(), new Color(98, 211, 168, 1));
+			Projectile.DrawMagicPixelTrail(Vector2.Zero, 4f, 1f, new Color(98, 211, 168, 255) * lightColor.GetLuminanceNTSC(), new Color(98, 211, 168, 1));
 
 			// draw circular glow
 			Texture2D glow = ModContent.Request<Texture2D>(Macrocosm.TextureAssetsPath + "Circle6").Value;
 			state.SaveState(Main.spriteBatch);
 			Main.spriteBatch.End();
 			Main.spriteBatch.Begin(BlendState.Additive, state);
-			Main.EntitySpriteDraw(glow, Projectile.Center - Main.screenPosition, null, new Color(89, 151, 193) * lightColor.GetLuminance(), 0f, glow.Size() / 2, 0.0375f, SpriteEffects.None);
+			Main.EntitySpriteDraw(glow, Projectile.Center - Main.screenPosition, null, new Color(89, 151, 193) * lightColor.GetLuminanceNTSC(), 0f, glow.Size() / 2, 0.0375f, SpriteEffects.None);
 			Main.spriteBatch.End();
 			Main.spriteBatch.Begin(state);
 
-			Main.EntitySpriteDraw(TextureAssets.Projectile[Type].Value, Projectile.Center - Main.screenPosition, null, Color.White * lightColor.GetLuminance(), 0f, TextureAssets.Projectile[Type].Size() / 2, Projectile.scale, SpriteEffects.None);
+			Main.EntitySpriteDraw(TextureAssets.Projectile[Type].Value, Projectile.Center - Main.screenPosition, null, Color.White * lightColor.GetLuminanceNTSC(), 0f, TextureAssets.Projectile[Type].Size() / 2, Projectile.scale, SpriteEffects.None);
 
 			return false;
 		}
