88e7c2384b78cfe1949302620d469bf13fd9954e
George Feldy <45595425+GeorgeFeldy@users.noreply.github.com>
2023-11-02 00:32:15 +0200

    some changes

--- Content/UI/UITheme.Static.cs
+++ Content/UI/UITheme.Static.cs
@@ -11,6 +11,29 @@ namespace Macrocosm.Content.UI
 		private static Dictionary<string, UITheme> themeStorage;
 
 		public static void Reload() => LoadThemes();
+		public void Load(Mod mod)
+		{
+			LoadThemes();
+		}
+
+		public void Unload()
+		{
+			themeStorage = null;
+		}
+
+		public static UITheme Current => Get(MacrocosmConfig.Instance.SelectedUITheme);
+
+		public static UITheme Get(string name)
+		{
+			if (themeStorage is null)
+				LoadThemes();
+
+			return themeStorage.TryGetValue(name, out var theme) ? theme : default;
+		}
+
+		public static void Add(UITheme theme)
+			=> themeStorage.Add(theme.Name, theme);
+
 		private static void LoadThemes()
 		{
 			themeStorage = new Dictionary<string, UITheme>();
@@ -25,47 +48,13 @@ namespace Macrocosm.Content.UI
 				WindowStyle = new(new Color(33, 43, 79) * 0.8f, Color.Black),
 				InfoElementStyle = new(new(57, 74, 136), Color.Black),
 				SeparatorColor = new Color(89, 116, 213),
-				ButtonStyle = new(new(57, 74, 136), Color.Black),
+				ButtonStyle = new(new(73, 85, 186), Color.Black),
 				ButtonHighlightStyle = new(new(69, 90, 166), Color.Gold),
 				InventorySlotStyle = new(Terraria.Main.inventoryBack, default)
 			});
 		}
 
-		public void Load(Mod mod)
-		{
-			LoadThemes();
-		}
 
-		public void Unload()
-		{
-			themeStorage = null;
-		}
-
-		public static void Add(UITheme theme)
-			=> themeStorage.Add(theme.Name, theme);
-
-		public static UITheme Current => Get(MacrocosmConfig.Instance.SelectedUITheme);
-
-		public static UITheme Get(string name) 
-		{
-			if (themeStorage is null)
-				LoadThemes();
-
-			return themeStorage.TryGetValue(name, out var theme) ? theme : default;
-		}
-
-		/*
-		public static string[] AvailableThemes
-		{
-			get
-			{
-				if (themeStorage is null)
-					LoadThemes();
-
-				return themeStorage.Keys.ToArray();
-			}
-		}
-		*/
 	}
 }
 
