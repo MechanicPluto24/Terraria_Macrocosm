513c199d0d1ab2b97167bb6c2f784bf0443ed990
Crystal Tucker <crystal.isabelle.tucker@gmail.com>
2023-12-05 07:03:19 -0600

    add: IConsistentUpdatable, impl for animated UI

--- Content/Rockets/UI/RocketUISystem.cs
+++ Content/Rockets/UI/RocketUISystem.cs
@@ -1,4 +1,5 @@
 ﻿using Macrocosm.Common.Config;
+using Macrocosm.Common.UI;
 using Macrocosm.Common.UI.Themes;
 using Macrocosm.Common.Utils;
 using Macrocosm.Content.Rockets.Customization;
@@ -8,146 +9,163 @@ using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Config.UI;
 using Terraria.UI;
 
 namespace Macrocosm.Content.Rockets.UI
 {
-    public class RocketUISystem : ModSystem
-    {
-        public static RocketUISystem Instance => ModContent.GetInstance<RocketUISystem>();
-        public UserInterface Interface { get; set; }
-        public RocketUIState UIRocketState { get; set; }
+	public class RocketUISystem : ModSystem
+	{
+		public static RocketUISystem Instance => ModContent.GetInstance<RocketUISystem>();
+		public UserInterface Interface { get; set; }
+		public RocketUIState UIRocketState { get; set; }
 
-        private GameTime lastGameTime;
+		private GameTime lastGameTime;
 
-        public static bool DebugModeActive { get; set; } = false;
+		public static bool DebugModeActive { get; set; } = false;
 
-        public static void Show(Rocket rocket) => Instance.ShowUI(rocket);
-        public static void Hide() => Instance.HideUI();
-        public static bool Active => Instance.Interface?.CurrentState is not null;
+		public static void Show(Rocket rocket) => Instance.ShowUI(rocket);
+		public static void Hide() => Instance.HideUI();
+		public static bool Active => Instance.Interface?.CurrentState is not null;
 
-        public override void Load()
-        {
-            if (Main.dedServ)
-                return;
+		public override void Load()
+		{
+			if (Main.dedServ)
+				return;
 
-            Interface = new UserInterface();
-            MacrocosmConfig.Instance.OnConfigChanged += OnConfigChanged;
-        }
+			Interface = new UserInterface();
+			MacrocosmConfig.Instance.OnConfigChanged += OnConfigChanged;
+		}
 
-        public override void Unload()
-        {
-            Interface = null;
-        }
+		public override void Unload()
+		{
+			Interface = null;
+		}
 
-        public override void OnWorldLoad()
-        {
-            if (Main.netMode == NetmodeID.Server)
-                return;
+		public override void OnWorldLoad()
+		{
+			if (Main.netMode == NetmodeID.Server)
+				return;
 
-            UIRocketState = new RocketUIState();
-            UIRocketState.Activate();
-        }
+			UIRocketState = new RocketUIState();
+			UIRocketState.Activate();
+		}
 
-        public override void OnWorldUnload()
-        {
-            if (Main.netMode == NetmodeID.Server)
-                return;
+		public override void OnWorldUnload()
+		{
+			if (Main.netMode == NetmodeID.Server)
+				return;
 
-            UIRocketState?.Deactivate();
-            UIRocketState = null;
-        }
+			UIRocketState?.Deactivate();
+			UIRocketState = null;
+		}
 
-        public void ResetUI()
-        {
-            Rocket rocket = new();
+		public override void PostUpdateEverything()
+		{
+			if (Interface.CurrentState is not null && UIRocketState is not null && UIRocketState.Rocket is not null)
+				UIRocketState.ExecuteRecursively(RecursiveUpdate);
+		}
 
-            if (UIRocketState is not null && UIRocketState.Rocket is not null)
-                rocket = UIRocketState.Rocket;
+		public void ResetUI()
+		{
+			Rocket rocket = new();
 
-            UIRocketState = new RocketUIState();
-            UIRocketState.Activate();
+			if (UIRocketState is not null && UIRocketState.Rocket is not null)
+				rocket = UIRocketState.Rocket;
 
-            if(Interface?.CurrentState != null)
-            {
-            HideUI();
-            ShowUI(rocket);
-        }
-        }
+			UIRocketState = new RocketUIState();
+			UIRocketState.Activate();
 
-        private void OnConfigChanged(object sender, System.EventArgs e)
-        {
-            ResetUI();
-        }
+			if (Interface?.CurrentState != null)
+			{
+				HideUI();
+				ShowUI(rocket);
+			}
+		}
 
-        public override void OnLocalizationsLoaded()
-        {
-        }
+		private void OnConfigChanged(object sender, System.EventArgs e)
+		{
+			ResetUI();
+		}
 
-        public void ShowUI(Rocket rocket)
-        {
-            if (Main.netMode == NetmodeID.Server || Interface.CurrentState is not null)
-                return;
+		public override void OnLocalizationsLoaded()
+		{
+		}
 
-            Main.playerInventory = true;
+		public void ShowUI(Rocket rocket)
+		{
+			if (Main.netMode == NetmodeID.Server || Interface.CurrentState is not null)
+				return;
 
-            UIRocketState.Rocket = rocket;
-            UIRocketState.OnShow();
+			Main.playerInventory = true;
 
-            Interface.SetState(UIRocketState);
-        }
+			UIRocketState.Rocket = rocket;
+			UIRocketState.OnShow();
 
-        public void HideUI()
-        {
-            UIRocketState?.OnHide();
+			Interface.SetState(UIRocketState);
+		}
 
-            if (Main.netMode != NetmodeID.Server)
-                Interface?.SetState(null);
-        }
+		public void HideUI()
+		{
+			UIRocketState?.OnHide();
 
-        public override void UpdateUI(GameTime gameTime)
-        {
-            // press Ctrl + Shift + E to reset UI
-            if (Interface.CurrentState is not null &&
-                Main.keyState.IsKeyDown(Keys.LeftControl) &&
-                Main.keyState.IsKeyDown(Keys.LeftShift) &&
-                Main.keyState.IsKeyDown(Keys.E) && !Main.oldKeyState.IsKeyDown(Keys.E))
-            {
-                //if(DebugModeActive) {
-                UITheme.Reload();
-                CustomizationStorage.Reset();
-                // }
+			if (Main.netMode != NetmodeID.Server)
+				Interface?.SetState(null);
+		}
 
-                ResetUI();
-                Utility.Chat("Reset rocket UI", Color.Lime);
-            }
 
-            lastGameTime = gameTime;
+		public override void UpdateUI(GameTime gameTime)
+		{
+			// press Ctrl + Shift + E to reset UI
+			if (Interface.CurrentState is not null &&
+				Main.keyState.IsKeyDown(Keys.LeftControl) &&
+				Main.keyState.IsKeyDown(Keys.LeftShift) &&
+				Main.keyState.IsKeyDown(Keys.E) && !Main.oldKeyState.IsKeyDown(Keys.E))
+			{
+				//if(DebugModeActive) {
+				UITheme.Reload();
+				CustomizationStorage.Reset();
+				// }
 
-            if (Interface?.CurrentState != null)
-            {
-                Main.LocalPlayer.mouseInterface = true;
-                Main.mouseRightRelease = false;
+				ResetUI();
+				Utility.Chat("Reset rocket UI", Color.Lime);
+			}
 
-                Interface.Update(gameTime);
-            }
-        }
+			lastGameTime = gameTime;
 
-        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
-        {
-            int mouseTextIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
-            if (mouseTextIndex != -1)
-            {
-                layers.Insert(mouseTextIndex, new LegacyGameInterfaceLayer(
-                    "Macrocosm:RocketUI",
-                    () =>
-                    {
-                        if (lastGameTime != null && Interface?.CurrentState != null)
-                            Interface.Draw(Main.spriteBatch, lastGameTime);
-                        return true;
-                    },
-                    InterfaceScaleType.UI));
-            }
-        }
-    }
+			if (Interface?.CurrentState != null)
+			{
+				Main.LocalPlayer.mouseInterface = true;
+				Main.mouseRightRelease = false;
+
+				Interface.Update(gameTime);
+			}
+		}
+
+		public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+		{
+			int mouseTextIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
+			if (mouseTextIndex != -1)
+			{
+				layers.Insert(mouseTextIndex, new LegacyGameInterfaceLayer(
+					"Macrocosm:RocketUI",
+					() =>
+					{
+						if (lastGameTime != null && Interface?.CurrentState != null)
+							Interface.Draw(Main.spriteBatch, lastGameTime);
+						return true;
+					},
+					InterfaceScaleType.UI));
+			}
+		}
+
+		private void RecursiveUpdate(UIElement element)
+		{
+			if (element is null)
+				return;
+
+			if (element is IConsistentUpdateable updateable)
+				updateable.Update();
+		}
+	}
 }
