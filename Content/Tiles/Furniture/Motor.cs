c4b6ceb9d252a553e9c8b5d57ce3757b3790ca8d
GeorgeFeldy <georgefeldy@gmail.com>
2023-12-15 23:53:14 +0200

    wip

---
+++ Content/Tiles/Furniture/Motor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Macrocosm.Content.Tiles.Furniture
+{
+	public class Motor : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoAttach[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+			TileObjectData.newTile.CoordinateWidth = 16;
+			TileObjectData.newTile.CoordinateHeights = new int[] { 16 };
+			TileObjectData.newTile.CoordinatePadding = 2;
+			TileObjectData.newTile.Direction = TileObjectDirection.PlaceRight;
+			TileObjectData.newTile.StyleHorizontal = false;
+			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+			TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceLeft;
+			TileObjectData.addAlternate(1);
+			TileObjectData.addTile(Type);
+
+			HitSound = SoundID.Dig;
+			DustType = -1;
+
+			AddMapEntry(new Color(0, 52, 154), CreateMapEntryName());
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+		}
+
+	}
+
+}
