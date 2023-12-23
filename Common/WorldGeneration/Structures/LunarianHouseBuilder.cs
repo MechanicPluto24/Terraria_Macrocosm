c10196a5d5acca338a62a806e116c783b557ae61
George Feldy <georgefeldy@gmail.com>
2023-12-24 00:04:03 +0200

    some base

---
+++ Common/WorldGeneration/Structures/LunarianHouseBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.WorldBuilding;
+
+namespace Macrocosm.Common.WorldGeneration.Structures
+{
+    public class LunarianHouseBuilder : MacrocosmHouseBuilder
+    {
+    }
+}
