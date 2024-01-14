650f74655dce78379eecf836bcf250d06fb87f7c
George Feldy <georgefeldy@gmail.com>
2024-01-14 18:01:49 +0200

    Moon Base plating wall, Shores of Oblivion painting, and extra debug commands

---
+++ Content/Items/Placeable/Paintings/ShoresOfOblivion.cs
@@ -0,0 +1,35 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Macrocosm.Content.Items.Placeable.Paintings
+{
+	public class ShoresOfOblivion : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 50;
+			Item.height = 34;
+			Item.maxStack = Item.CommonMaxStack;
+			Item.value = 0;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.useTurn = true;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.autoReuse = true;
+			Item.consumable = true;
+			Item.createTile = TileType<Tiles.Paintings.ShoresOfOblivion>();
+			Item.placeStyle = 0;
+		}
+
+		public override void AddRecipes()
+		{
+
+		}
+	}
+}
