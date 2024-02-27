6520eb385bb635fa60a296fac68d3328fb98c1be
George Feldy <georgefeldy@gmail.com>
2024-02-27 04:04:10 +0200

    MUSIC BOXES

---
+++ Content/Tiles/MusicBoxes/StygiaMusicBox.cs
@@ -0,0 +1,35 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Macrocosm.Content.Tiles.MusicBoxes
+{
+	public class StygiaMusicBox : ModTile
+	{
+		public override void SetStaticDefaults() 
+		{
+			Main.tileFrameImportant[Type] = true;
+			Main.tileObsidianKill[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+			TileObjectData.newTile.Origin = new Point16(0, 1);
+			TileObjectData.newTile.LavaDeath = false;
+			TileObjectData.newTile.DrawYOffset = 2;
+			TileObjectData.newTile.StyleLineSkip = 2;
+			TileObjectData.addTile(Type);
+
+			DustType = -1;
+
+			AddMapEntry(new Color(92, 228, 162), CreateMapEntryName());
+		}
+
+		public override void MouseOver(int i, int j)
+		{
+			Player player = Main.LocalPlayer;
+			player.noThrow = 2;
+			player.cursorItemIconEnabled = true;
+			player.cursorItemIconID = ModContent.ItemType<Items.Placeable.MusicBoxes.StygiaMusicBox>();
+		}
+	}
+}
