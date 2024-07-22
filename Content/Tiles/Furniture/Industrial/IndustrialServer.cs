fd811c5e38a99c73ae981769c8448f8d9a502361
GeorgeFeldy <georgefeldy@gmail.com>
2024-07-23 00:28:38 +0300

    added stuffs

---
+++ Content/Tiles/Furniture/Industrial/IndustrialServer.cs
@@ -0,0 +1,33 @@
+﻿using Macrocosm.Content.Dusts;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Macrocosm.Content.Tiles.Furniture.Industrial
+{
+    [LegacyName("MoonBaseServer")]
+    public class IndustrialServer : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
+            TileObjectData.newTile.Width = 2;
+            TileObjectData.newTile.Height = 4;
+            TileObjectData.newTile.CoordinateHeights = [16, 16, 16, 16];
+            TileObjectData.newTile.DrawYOffset = 2;
+            TileObjectData.addTile(Type);
+
+            HitSound = SoundID.Dig;
+
+            DustType = ModContent.DustType<IndustrialPlatingDust>();
+
+            AddMapEntry(new Color(200, 200, 200), CreateMapEntryName());
+        }
+    }
+}
