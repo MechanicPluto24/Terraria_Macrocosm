ba37c600df7fe412db35f520131f0faaf0efd53f
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-08-12 16:29:54 -0400

    PAIN

---
+++ Content/Debuffs/RadDebuffs/MildNecrosis.cs
@@ -0,0 +1,19 @@
+using Terraria;
+using Terraria.ModLoader;
+using  Macrocosm.Content.Players;
+namespace Macrocosm.Content.Debuffs
+{
+    public class MildNecrosis : ModBuff.RadDebuffs
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.debuff[Type] = true;
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = true;
+        }
+        public override void Update(Player player, ref int buffIndex)
+        {
+           player.GetDamage<GenericDamageClass>() *=1f*(1/(player.GetModPlayer<IrradiationPlayer>().IrradiationLevels+1f));
+        }
+    }
+}
