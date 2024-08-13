127866ee45a50e732bd383e645f5a08bee19b1b1
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-08-13 17:14:57 -0400

    Add files via upload

---
+++ Content/NPCs/Enemies/Moon/MoonLich/MoonLichNpcSummon.cs
@@ -0,0 +1,80 @@
+using Macrocosm.Common.DataStructures;
+using Macrocosm.Common.Utils;
+using Macrocosm.Content.Dusts;
+using Macrocosm.Content.NPCs.Enemies.Moon;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Macrocosm.Content.Particles;
+using Macrocosm.Common.Drawing.Particles;
+namespace Macrocosm.Content.NPCs.Enemies.Moon.MoonLich
+{
+    public class MoonLichNPCSummon : ModProjectile
+    {
+        public override string Texture => Macrocosm.EmptyTexPath;
+
+        public override void SetStaticDefaults()
+        {
+     
+        }
+        Color colour = new Color(100,255,255);
+
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 90;
+            Projectile.height = 90;
+            Projectile.hostile = false;
+            Projectile.friendly = false;
+            Projectile.tileCollide = false;
+            Projectile.timeLeft = 900;
+            Projectile.penetrate = -1;
+
+            Projectile.tileCollide = true;
+        }
+
+        private SpriteBatchState state;
+        public override bool PreDraw(ref Color lightColor)
+        {
+            return false;
+        }
+        bool Summon=false;
+        int Timer=0;
+         public override bool OnTileCollide(Vector2 oldVelocity){
+            Summon=true;  
+            return false;
+        }
+        public override void AI()
+        {
+            Particle.CreateParticle<TintableFire>(p =>
+                            {
+                                p.Position = Projectile.position;
+                                p.Velocity = Vector2.UnitX.RotatedByRandom(MathHelper.TwoPi);
+                                p.DrawColor = new Color(100,255,255,0);
+                                p.Scale=0.03f;
+                            });
+        Projectile.velocity.Y+=1f;
+        if (Summon==true){
+            Projectile.velocity*=0f;
+            Timer++;
+        }
+        if (Timer>30)
+        {
+        NPC.NewNPCDirect(Projectile.GetSource_FromAI(),(int)Projectile.Center.X,(int)Projectile.Center.Y,ModContent.NPCType<XenoHive>(), 0, 0f);
+        Projectile.Kill();
+        }
+
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Vector2 dustVelocity = Utility.PolarVector(0.01f, Utility.RandomRotation());
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<LuminiteDust>(), dustVelocity.X, dustVelocity.Y, newColor: Color.White * 0.1f);
+            }
+        }
+    }
+}
---
+++ Content/NPCs/Enemies/Moon/MoonLich/XenoHornet.cs
@@ -0,0 +1,100 @@
+using Macrocosm.Common.Global.NPCs;
+using Macrocosm.Common.Utils;
+using Macrocosm.Content.Biomes;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Macrocosm.Content.NPCs.Enemies.Moon.MoonLich
+{
+
+
+    public class XenoHornet : ModNPC, IMoonEnemy
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            Main.npcFrameCount[Type] = 5;
+        }
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+
+            NPC.width = 34;
+            NPC.height = 34;
+            NPC.damage = 80;
+            NPC.defense = 30;
+            NPC.lifeMax = 200;
+          
+            NPC.knockBackResist = 0.3f;
+            NPC.aiStyle = -1;
+       
+            
+        }
+
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[]
+            {
+                new FlavorTextBestiaryInfoElement(
+                    " ")
+            });
+        }
+        public float ChasingVel=1f;
+        public override void AI()//Basic AI
+        {
+            NPC.TargetClosest(true);
+        Player target = Main.player[NPC.target];
+        ChasingVel+=0.08f;
+        if (ChasingVel>12f)
+            ChasingVel=12f;
+        Vector2 Homing=(target.Center-NPC.Center).SafeNormalize(Vector2.UnitX);
+        NPC.velocity=((NPC.velocity+(Homing*0.8f)).SafeNormalize(Vector2.UnitX))*ChasingVel;
+    
+        }
+      
+       
+
+        public override void FindFrame(int frameHeight)
+        {
+            int ticksPerFrame = 3;
+            NPC.frameCounter++;
+            NPC.spriteDirection = NPC.direction;
+
+            
+                if (NPC.frameCounter >= ticksPerFrame)
+                {
+                    NPC.frameCounter = 0;
+                    NPC.frame.Y += frameHeight;
+
+                    if (NPC.frame.Y >= 5 * frameHeight - 1)
+                        NPC.frame.Y = 1 * frameHeight;
+                }
+            
+        
+        }
+
+        public override void ModifyNPCLoot(NPCLoot loot)
+        {
+        }
+
+        public override void HitEffect(NPC.HitInfo hit)
+        {
+
+            
+            
+                for (int i = 0; i < 20; i++)
+                {
+                    Dust dust = Dust.NewDustDirect(NPC.position, NPC.width, NPC.height, DustID.CorruptGibs);
+                    dust.velocity.X = (dust.velocity.X + Main.rand.Next(0, 100) * 0.02f) * hit.HitDirection;
+                    dust.velocity.Y = 1f + Main.rand.Next(-50, 51) * 0.01f;
+                    dust.scale *= 1f + Main.rand.Next(-30, 31) * 0.01f;
+                    dust.noGravity = true;
+                }
+            }
+           
+        }
+    }
