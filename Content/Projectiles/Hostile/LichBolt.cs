d7492ff360dd30fbf1b5fff3acdfb22daf3a823c
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-08-13 17:15:38 -0400

    Add files via upload

---
+++ Content/Projectiles/Hostile/LichBolt.cs
@@ -0,0 +1,71 @@
+using Macrocosm.Common.DataStructures;
+using Macrocosm.Common.Utils;
+using Macrocosm.Content.Dusts;
+using Macrocosm.Content.NPCs.Enemies.Moon;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Macrocosm.Content.Projectiles.Hostile
+{
+    public class LichBolt : ModProjectile
+    {
+        public override string Texture => Macrocosm.EmptyTexPath;
+
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Type] = 15;
+            ProjectileID.Sets.TrailingMode[Type] = 0;
+        }
+        //Similar to the luminite star.
+        Color colour = new Color(100,255,255);
+
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 8;
+            Projectile.height = 8;
+            Projectile.hostile = true;
+            Projectile.friendly = false;
+            Projectile.tileCollide = false;
+            Projectile.timeLeft = 600;
+            Projectile.penetrate = -1;
+
+            Projectile.tileCollide = true;
+        }
+
+        private SpriteBatchState state;
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Projectile.DrawMagicPixelTrail(Vector2.Zero, 5f, 1f, colour, colour.WithOpacity(0f));
+
+            state.SaveState(Main.spriteBatch);
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(BlendState.Additive, state);
+
+            Main.spriteBatch.DrawStar(Projectile.Center - Main.screenPosition, 2, colour, 0.4f, Projectile.rotation, entity: true);
+
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(state);
+
+
+            return false;
+        }
+
+        public override void AI()
+        {
+        Projectile.velocity*=1.01f;
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Vector2 dustVelocity = Utility.PolarVector(0.01f, Utility.RandomRotation());
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<LuminiteDust>(), dustVelocity.X, dustVelocity.Y, newColor: Color.White * 0.1f);
+            }
+        }
+    }
+}
