a9ddf4cbf19581372e2a7a374efe3706752fc293
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-08-23 16:42:36 -0400

    Lexan Things

---
+++ Content/Items/Walls/LexanGlassWall.cs
@@ -0,0 +1,32 @@
+using Macrocosm.Content.Items.Blocks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Macrocosm.Content.Items.Walls
+{
+
+    public class LexanGlassWall : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 400;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableWall(ModContent.WallType<Tiles.Walls.LexanGlassWall>());
+            Item.width = 24;
+            Item.height = 24;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(4)
+                .AddIngredient<LexanGlass>()
+                .AddTile(TileID.WorkBenches)
+                .DisableDecraft()
+                .Register();
+        }
+    }
+}
