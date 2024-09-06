e87685a9e1df5d80521d90651199f77ce9cca559
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-09-06 18:01:51 -0400

    Wave guns

--- Content/Items/Weapons/Magic/WaveGuns.cs
+++ Content/Items/Weapons/Magic/WaveGuns.cs
@@ -1,46 +1,87 @@
-using Macrocosm.Content.Projectiles.Friendly.Magic;
+using Macrocosm.Content.Projectiles.Friendly.Magic.WaveGuns;
 using Macrocosm.Content.Rarities;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Macrocosm.Content.Items.Weapons.Magic
 {
-    public class WaveGunSingle : ModItem
+    
+    public class WaveGunBlue : ModItem
     {
+       
         public override void SetStaticDefaults()
         {
 
 
         }
-        public override string Texture => Macrocosm.EmptyTexPath;
-
         public override void SetDefaults()
         {
-            Item.damage = 200;
+            Item.damage = 300;
             Item.DamageType = DamageClass.Magic;
             Item.mana = 10;
             Item.width = 54;
             Item.height = 36;
-            Item.useTime = 34;
-            Item.useAnimation = 34;
+            Item.useTime = 20;
+            Item.noUseGraphic = true;
+            Item.useAnimation = 20;
             Item.useStyle = ItemUseStyleID.Shoot;
             Item.noMelee = true;
             Item.knockBack = 10;
             Item.value = Item.sellPrice(0, 5, 0, 0);
-            Item.rare = ModContent.RarityType<MoonRarityT1>();
+            Item.rare = ModContent.RarityType<MoonRarityT2>();
             Item.UseSound = SoundID.Item11;
             Item.autoReuse = true;
-            Item.shoot = ModContent.ProjectileType<WaveGunLaser>();
+            Item.shoot = ModContent.ProjectileType<BlueEnergyBolt>();
             Item.shootSpeed = 28f;
+            Item.channel = true;
         }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+        
+            Vector2 aim = velocity;
+            Projectile.NewProjectileDirect(source, position, aim, ModContent.ProjectileType<BlueGunHeld>(), damage, knockback, player.whoAmI);
+            return false;
+        }
+        
     }
-    public class WaveGunBlue : WaveGunSingle
+    public class WaveGunRed : ModItem
     {
-        public override string Texture => "Macrocosm/Content/Items/Weapons/Magic/WaveGunBlue";
-    }
-    public class WaveGunRed : WaveGunSingle
-    {
-        public override string Texture => "Macrocosm/Content/Items/Weapons/Magic/WaveGunRed";
+     
+        public override void SetStaticDefaults()
+        {
+
+
+        }
+        public override void SetDefaults()
+        {
+            Item.damage = 300;
+            Item.DamageType = DamageClass.Magic;
+            Item.mana = 10;
+            Item.width = 54;
+            Item.height = 36;
+            Item.useTime = 20;
+            Item.noUseGraphic = true;
+            Item.useAnimation = 20;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.noMelee = true;
+            Item.knockBack = 10;
+            Item.value = Item.sellPrice(0, 5, 0, 0);
+            Item.rare = ModContent.RarityType<MoonRarityT2>();
+            Item.UseSound = SoundID.Item11;
+            Item.autoReuse = true;
+            Item.shoot = ModContent.ProjectileType<RedEnergyBolt>();
+            Item.shootSpeed = 28f;
+            Item.channel = true;
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+        
+            Vector2 aim = velocity;
+            Projectile.NewProjectileDirect(source, position, aim, ModContent.ProjectileType<RedGunHeld>(), damage, knockback, player.whoAmI);
+            return false;
+        }
     }
 }
