dfe227dc052f92a7e278b16d546dd63ec83a2ed8
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-09-14 17:12:35 -0400

    Fixes and stuff

--- Content/Projectiles/Friendly/Melee/ChampionsBladeSlash.cs
+++ Content/Projectiles/Friendly/Melee/ChampionsBladeSlash.cs
@@ -1,73 +1,149 @@
-using Macrocosm.Content.Dusts;
+using Macrocosm.Common.DataStructures;
+using Macrocosm.Common.Drawing.Particles;
+using Macrocosm.Common.Utils;
+using Macrocosm.Content.Particles;
+using Macrocosm.Content.Trails;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria;
 using Terraria.GameContent;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Macrocosm.Content.Projectiles.Friendly.Melee
 {
     public class ChampionsBladeSlash : ModProjectile
     {
+        public override string Texture => Macrocosm.TexturesPath + "Swing";
+
         public override void SetStaticDefaults()
         {
+            Main.projFrames[Type] = 4;
+            ProjectileID.Sets.TrailCacheLength[Type] = 15;
+            ProjectileID.Sets.TrailingMode[Type] = 3;
         }
 
-        public ref float ProjectileType => ref Projectile.ai[0];
+        public ref float Timer => ref Projectile.ai[0];
         public ref float Speed => ref Projectile.ai[1];
+
+        private ChampionsBladeTrail trail;
 
         public override void SetDefaults()
         {
-            Projectile.width = 50;
-            Projectile.height = 50;
-            Projectile.aiStyle = -1;
+            Projectile.width = 64;
+            Projectile.height = 64;
             Projectile.friendly = true;
             Projectile.DamageType = DamageClass.Melee;
+            Projectile.penetrate = -1;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
             Projectile.tileCollide = false;
             Projectile.ignoreWater = true;
-            Projectile.penetrate = 1;
-            Projectile.timeLeft = 120;
+            Projectile.aiStyle = -1;
             Projectile.Opacity = 0f;
+            trail = new();
         }
+
+        /*
+        public ref float SwingDirection => ref Projectile.ai[0];
+        public ref float MaxTime => ref Projectile.ai[1];
+        public ref float Scale => ref Projectile.ai[2];
+        */
 
         public override void AI()
         {
-            Player player = Main.player[Projectile.owner];
-            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+            Projectile.direction = Projectile.velocity.X > 0f ? 1 : -1;
+            Projectile.spriteDirection = Projectile.direction;
+            Projectile.rotation = Projectile.velocity.ToRotation() + 0.2f * Projectile.direction;
+            Projectile.velocity = Projectile.velocity.SafeNormalize(Vector2.UnitY) * Speed;
+
             if (Projectile.Opacity < 1f)
-                Projectile.Opacity += 0.02f;
-            if (ProjectileType == 0f)
-                Speed *= 0.99f;
-            if (ProjectileType == 1f && Speed < 28f)
-                Speed *= 1.01f;
-            Projectile.velocity = Projectile.velocity.SafeNormalize(Vector2.UnitX) * Speed;
+                Projectile.Opacity += 0.01f;
+
+            trail.WidthMult = Utility.BackEaseInOut(Projectile.Opacity);
+
+            if (Timer >= 30 && Speed < 45f)
+                Speed *= 1.2f;
+
+            Timer++;
         }
 
-        public override void OnKill(int timeLeft)
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
         {
-            for (int i = 0; i < 15; i++)
-            {
-                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<LuminiteBrightDust>());
-                dust.velocity.X = Main.rand.Next(-30, 31) * 0.02f;
-                dust.velocity.Y = Main.rand.Next(-30, 30) * 0.02f;
-                dust.scale *= 1f + Main.rand.Next(-12, 13) * 0.01f;
-                dust.noGravity = true;
-            }
+            return null;
         }
+
+        public override void CutTiles()
+        {
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            Particle.CreateParticle<ArtemiteStar>((p) =>
+            {
+                p.Position = target.Center;
+                p.Velocity = -Vector2.UnitY * 0.4f;
+                p.Scale = 1f;
+                p.Rotation = MathHelper.PiOver4;
+            }, shouldSync: true
+            );
+        }
+
+        public override void OnHitPlayer(Player target, Player.HurtInfo info)
+        {
+            Particle.CreateParticle<ArtemiteStar>((p) =>
+            {
+                p.Position = target.Center;
+                p.Velocity = -Vector2.UnitY * 0.4f;
+                p.Scale = 1f;
+                p.Rotation = MathHelper.PiOver4;
+            }, shouldSync: true
+            );
+        }
+
+        private SpriteBatchState state;
         public override bool PreDraw(ref Color lightColor)
         {
+            ProjectileID.Sets.TrailCacheLength[Type] = 30;
+            ProjectileID.Sets.TrailingMode[Type] = 3;
 
+            state.SaveState(Main.spriteBatch);
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(BlendState.Additive, state);
+
+            trail.Opacity = Projectile.Opacity;
+            trail?.Draw(Projectile, Projectile.Size * 0.6f);
+
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(state);
+
+            Vector2 positionOffset = (Projectile.velocity.SafeNormalize(Vector2.UnitY)) * -100f;
+            Vector2 position = (Projectile.Center + positionOffset) - Main.screenPosition;
             Texture2D texture = TextureAssets.Projectile[Type].Value;
-            Vector2 origin = texture.Size() / 2f;
-            float multiplier = 1f;
+            Vector2 origin = texture.Frame(1, 4).Size() / 2f;
+            float scale = Projectile.scale * 1.2f;
+            SpriteEffects spriteEffects = ((!(Projectile.velocity.X >= 0f)) ? SpriteEffects.FlipVertically : SpriteEffects.None); // Flip the sprite based on the direction it is facing.
+            float progress = 0.5f;
+            float lightingColor = Lighting.GetColor(Projectile.Center.ToTileCoordinates()).ToVector3().Length() / (float)Math.Sqrt(3.0);
+            lightingColor = Utils.Remap(lightingColor, 0.2f, 1f, 0f, 1f);
+            float progressScale = Utils.Remap(progress, 0f, 0.6f, 0f, 1f) * Utils.Remap(progress, 0.6f, 1f, 1f, 0f);
 
-            for (int i = 0; i < 10; i++)
+            Color color = new Color(30, 255, 105) * 1.4f * Projectile.Opacity;
+            Color middleMediumColor = color;
+
+            Main.EntitySpriteDraw(texture, position, texture.Frame(1, 4, frameY: 2), middleMediumColor * 1f, Projectile.rotation + Projectile.spriteDirection * MathHelper.PiOver4 * -0.5f * (1f - progress), origin, scale * 1.05f, spriteEffects, 0f);
+            Main.EntitySpriteDraw(texture, position, texture.Frame(1, 4, frameY: 2), middleMediumColor * 0.6f, Projectile.rotation + Projectile.spriteDirection * MathHelper.PiOver4 * -1f * (1f - progress), origin, scale * 1.05f, spriteEffects, 0f);
+            Main.EntitySpriteDraw(texture, position, texture.Frame(1, 4, frameY: 2), middleMediumColor * 0.2f, Projectile.rotation + Projectile.spriteDirection * MathHelper.PiOver4 * -1.5f * (1f - progress), origin, scale * 1.05f, spriteEffects, 0f);
+
+            // This draws some sparkles around the circumference of the swing.
+            for (float i = 0f; i < 14f; i += 1f)
             {
-                Vector2 position = (Projectile.Center - (Projectile.velocity.SafeNormalize(Vector2.UnitX) * 15f * i)) - Main.screenPosition;
-                Main.EntitySpriteDraw(texture, position, null, new Color(255, 255, 255, 0) * Projectile.Opacity * multiplier, Projectile.rotation, origin, Projectile.scale * multiplier * 2.5f, SpriteEffects.None, 0f);
-                multiplier -= 0.1f;
+                float edgeRotation = Projectile.rotation + Projectile.spriteDirection * i * (MathHelper.Pi * -2f) * 0.02f + Utils.Remap(progress, 0f, 1f, 0f, MathHelper.PiOver4 * 1.6f) * Projectile.spriteDirection;
+                Vector2 drawPos = position + edgeRotation.ToRotationVector2() * ((float)texture.Width * 0.5f - 1f) * scale;
+                Utility.DrawPrettyStarSparkle(Projectile.Opacity, SpriteEffects.None, drawPos, color.WithOpacity(Projectile.Opacity) * (i / 9f), middleMediumColor, progress, 0f, 0.5f, 0.5f, 1f, edgeRotation, new Vector2(0f, Utils.Remap(progress, 0f, 1f, 2f, 0f)) * scale, Vector2.One * scale);
             }
-
+                
             return false;
         }
     }
