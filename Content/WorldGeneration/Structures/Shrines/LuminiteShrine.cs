59e070eea92cf0ea96e822cb75eb941aeec7a1e2
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-09-14 13:35:28 -0400

    Benches, enemies, and a shrine

---
+++ Content/WorldGeneration/Structures/Shrines/LuminiteShrine.cs
@@ -0,0 +1,32 @@
+﻿using Macrocosm.Common.WorldGeneration;
+using Macrocosm.Content.Tiles.Ores;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Macrocosm.Content.WorldGeneration.Structures.Shrines
+{
+    public class LuminiteShrine : Structure
+    {
+        public override bool PrePlace(Point16 origin)
+        {
+            return true;
+        }
+
+        public override void PostPlace(Point16 origin)
+        {
+            int max = WorldGen.genRand.Next(15, 26);
+            for (int vein = 0; vein < max; vein++)
+            {
+                WorldGen.OreRunner(
+                    i: origin.X + (int)(Size.X * WorldGen.genRand.NextFloat(1f)),
+                    j: origin.Y + (int)(Size.Y * WorldGen.genRand.NextFloat(1f)),
+                    strength: WorldGen.genRand.Next(2, 5),
+                    steps: WorldGen.genRand.Next(4, 8),
+                    type: TileID.LunarOre
+                );
+            }
+        }
+    }
+}
