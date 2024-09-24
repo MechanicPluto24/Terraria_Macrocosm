ef0a03dc636b16bf342d68966e71c36426ba6c8d
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-09-24 17:18:28 -0400

    Things

---
+++ Content/Items/Ores/QuartzFragment.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Macrocosm.Content.Rarities;
+
+namespace Macrocosm.Content.Items.Ores
+{
+    public class QuartzFragment : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.value = 1000;
+            Item.rare = ModContent.RarityType<MoonRarityT1>();
+            Item.material = true;
+        }
+
+    }
+}
