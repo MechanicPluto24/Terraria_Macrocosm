555a45bd74324c750d8a174778c722a3db2c5d75
sol4ar <83139747+sol4ar@users.noreply.github.com>
2024-10-03 14:17:32 +0200

    champions blade thing

---
+++ Content/Projectiles/Friendly/Melee/ChampionsBladeBoltProjectile.cs
@@ -0,0 +1,74 @@
+﻿using Macrocosm.Common.Drawing.Trails;
+using Macrocosm.Common.Utils;
+using Macrocosm.Content.Trails;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Macrocosm.Content.Projectiles.Friendly.Melee
+{
+    internal class ChampionsBladeBoltProjectile : ModProjectile
+    {
+        public override string Texture => Macrocosm.EmptyTexPath;
+
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Type] = 30;
+            ProjectileID.Sets.TrailingMode[Type] = 3;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 10;
+            Projectile.height = 10;
+
+            Projectile.aiStyle = -1;
+
+            Projectile.DamageType = DamageClass.Melee;
+
+            Projectile.penetrate = 1;
+
+            Projectile.timeLeft = 80;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = true;
+
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 999;
+
+            trail = new MoonSwordTrail();
+        }
+
+        private ref float SkewMultiplier => ref Projectile.ai[0];
+        private VertexTrail trail;
+        public override void OnSpawn(IEntitySource source)
+        {
+            SkewMultiplier = (Main.rand.NextBool() ? 1f : -1f) * Main.rand.NextFloat(1.5f);
+            Projectile.netUpdate = true;
+        }
+
+        public override void AI()
+        {
+            Projectile.velocity = Projectile.velocity.RotatedBy(SkewMultiplier * 0.05f);
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            var state = Main.spriteBatch.SaveState();
+
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(BlendState.Additive, state);
+
+            trail?.Draw(Projectile, new Vector2(24 * Projectile.direction, 4).RotatedBy(Projectile.rotation) + Projectile.Size * 0.5f);
+
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(state);
+            return false;
+        }
+    }
+}
---
+++ Content/Projectiles/Friendly/Melee/ChampionsBladeHeldProjectile.cs
@@ -0,0 +1,137 @@
+﻿using Macrocosm.Content.Items.Weapons.Melee;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ModLoader;
+
+namespace Macrocosm.Content.Projectiles.Friendly.Melee
+{
+    internal class ChampionsBladeHeldProjectile : ModProjectile
+    {
+        public override string Texture => "Macrocosm/Content/Items/Weapons/Melee/ChampionsBlade";
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 0;
+            Projectile.height = 0;
+
+            Projectile.aiStyle = -1;
+
+            Projectile.DamageType = DamageClass.Melee;
+
+            Projectile.penetrate = -1;
+
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = false;
+
+            Projectile.extraUpdates = 2;
+
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 999;
+        }
+
+        private Player Player => Main.player[Projectile.owner];
+        private ref float Arc => ref Projectile.ai[1];
+        private ref float LastRotation => ref Projectile.ai[0];
+        private ChampionsBlade item;
+
+        // So that the weapon doesn't "blink" during continuous use.
+        private bool despawn;
+
+        public override void OnSpawn(IEntitySource source)
+        {
+            // we just gon assume this true !
+            item = (source as EntitySource_ItemUse_WithAmmo).Item.ModItem as ChampionsBlade;
+
+            item.lastDirection = -item.lastDirection;
+            LastRotation = item.lastRotation;
+            Arc = Projectile.velocity.ToRotation() + item.lastDirection * Main.rand.NextFloat(MathHelper.PiOver2, MathHelper.Pi) - item.lastRotation;
+
+            Projectile.netUpdate = true;
+        }
+
+        public override bool ShouldUpdatePosition()
+        {
+            return false;
+        }
+
+        public override void AI()
+        {
+            if (!despawn)
+            {
+                Projectile.timeLeft = 2;
+            }
+
+            Player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, Projectile.rotation - MathHelper.PiOver2);
+            Player.heldProj = Projectile.whoAmI;
+            Projectile.Center = Player.RotatedRelativePoint(Player.MountedCenter) + new Vector2(Player.direction * -3, -1);
+
+            if (Player.noItems || Player.CCed || Player.ItemAnimationEndingOrEnded || Player.HeldItem.type != ModContent.ItemType<ChampionsBlade>())
+            {
+                despawn = true;
+                if (item is not null)
+                {
+                    item.lastRotation = Projectile.rotation;
+                }
+
+                return;
+            }
+
+            var x = 1f - (float)Player.itemAnimation / Player.itemAnimationMax - 1f;
+            Projectile.rotation = LastRotation + Arc * (MathF.Sin((x * x - 0.5f) * MathHelper.Pi) + 1f) / 2f;
+
+
+            /*            if (Main.netMode != NetmodeID.MultiplayerClient)
+                        {
+
+                            if (Main.rand.NextBool(30))
+                            {
+                                Projectile.NewProjectile(
+                                    Projectile.GetSource_FromAI(),
+                                    Projectile.Center,
+                                    Projectile.velocity.SafeNormalize(Vector2.Zero) * 23f,
+                                    ModContent.ProjectileType<ChampionsBladeBoltProjectile>(),
+                                    40,
+                                    2f);
+                            }
+                        }
+            */
+        }
+
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            return Collision.CheckAABBvLineCollision(
+                targetHitbox.TopLeft(),
+                targetHitbox.Size(),
+                Projectile.Center,
+                Projectile.Center + Projectile.rotation.ToRotationVector2() * 80f);
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            var texture = TextureAssets.Projectile[Type].Value;
+            var rotation = Projectile.rotation + (Player.direction == 1 ? MathHelper.PiOver4 : MathHelper.Pi * 0.75f);
+            var origin = new Vector2(Player.direction == 1 ? 10 : 67, 67);
+
+            Main.spriteBatch.Draw(
+                texture,
+                Projectile.Center + Projectile.rotation.ToRotationVector2() * 16f - Main.screenPosition,
+                null,
+                lightColor,
+                rotation,
+                origin,
+                Projectile.scale,
+                Player.direction == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally,
+                0
+            );
+
+            return false;
+        }
+    }
+}
