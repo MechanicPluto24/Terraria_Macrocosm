f97750269b8c7d7485227b185f7841cbbacedd12
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-10-04 18:58:56 -0400

    I need to update my branch lol

--- Content/Projectiles/Friendly/Melee/LuminiteRune.cs
+++ Content/Projectiles/Friendly/Melee/LuminiteRune.cs
@@ -1,8 +1,11 @@
-using Macrocosm.Content.Dusts;
+using Macrocosm.Common.DataStructures;
+using Macrocosm.Common.Utils;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Macrocosm.Content.Dusts;
 
 namespace Macrocosm.Content.Projectiles.Friendly.Melee
 {
@@ -11,7 +14,10 @@ namespace Macrocosm.Content.Projectiles.Friendly.Melee
         public override void SetStaticDefaults()
         {
             Main.projFrames[Type] = 4;
+            ProjectileID.Sets.TrailCacheLength[Type] = 15;
+            ProjectileID.Sets.TrailingMode[Type] =3;
         }
+        private float originalSpeed;
         public override void SetDefaults()
         {
             Projectile.width = 10;
@@ -25,13 +31,11 @@ namespace Macrocosm.Content.Projectiles.Friendly.Melee
             Projectile.timeLeft = 600;
         }
 
-        private float originalSpeed;
-        private bool spawned;
-        private float rotationSpeed;
+        bool spawned;
+        float rotationSpeed;
+        Color colour;
         private int targetNPC;
-
         private NPC TargetNPC => Main.npc[targetNPC];
-
         public override void AI()
         {
             Player player = Main.player[Projectile.owner];
@@ -40,54 +44,63 @@ namespace Macrocosm.Content.Projectiles.Friendly.Melee
             {
                 rotationSpeed = 0.15f;
                 Projectile.rotation += rotationSpeed;
-                Projectile.frame = Main.rand.Next(0, 4);
+                Projectile.frame=Main.rand.Next(0,4);
                 spawned = true;
+                colour= Projectile.frame<2 ? new Color(94,229,163,255) : new Color(213,155,163,148);
             }
 
             Projectile.rotation += rotationSpeed;
             rotationSpeed *= 1f - 0.015f;
-            Projectile.Opacity -= 0.003f;
+            Projectile.Opacity -= 0.001f;
 
             if (WorldGen.SolidTile(Projectile.Center.ToTileCoordinates()))
+            {
                 rotationSpeed *= 1f - 0.01f;
-
-            if (Projectile.Opacity < 0.01f)
+                
+            }
+            if (Projectile.Opacity<0.01f){
                 Projectile.Kill();
-
+            }
             originalSpeed = Projectile.velocity.Length();
             float closestDistance = 3000f;
-
-            for (int i = 0; i < Main.maxNPCs; i++)
-            {
-                NPC npc = Main.npc[i];
-                if (npc.active && !npc.friendly && npc.lifeMax > 5 && !npc.dontTakeDamage && npc.type != NPCID.TargetDummy)
-                {
-                    float distance = Vector2.Distance(Projectile.Center, npc.Center);
-                    if (distance < closestDistance)
+             for (int i = 0; i < Main.maxNPCs; i++)
                     {
-                        closestDistance = distance;
-                        targetNPC = npc.whoAmI;
+                        NPC npc = Main.npc[i];
+                        if (npc.active && !npc.friendly && npc.lifeMax > 5 && !npc.dontTakeDamage && npc.type != NPCID.TargetDummy)
+                        {
+                            float distance = Vector2.Distance(Projectile.Center, npc.Center);
+                            if (distance < closestDistance)
+                            {
+                                closestDistance = distance;
+                                targetNPC = npc.whoAmI;
+                            }
+                        }
                     }
-                }
-            }
-
-            if (TargetNPC is not null && Vector2.Distance(Projectile.Center, TargetNPC.Center) < 3000f && TargetNPC.active && !TargetNPC.friendly && TargetNPC.lifeMax > 5 && !TargetNPC.dontTakeDamage)
-            {
-                Vector2 direction = (TargetNPC.Center - Projectile.Center).SafeNormalize(Vector2.Zero);
-                Projectile.velocity = Vector2.Lerp(Projectile.velocity, direction * originalSpeed, 0.02f);
-                Projectile.timeLeft--;
+                    if (TargetNPC is not null && Vector2.Distance(Projectile.Center, TargetNPC.Center) < 3000f && TargetNPC.active && !TargetNPC.friendly && TargetNPC.lifeMax > 5 && !TargetNPC.dontTakeDamage)
+                    {
+                        Vector2 direction = (TargetNPC.Center - Projectile.Center).SafeNormalize(Vector2.Zero);
+                        Projectile.velocity = Vector2.Lerp(Projectile.velocity, direction * originalSpeed, 0.07f);
+                        Projectile.timeLeft--;
+                    }
+    
+            
+        }
+        public override void OnKill(int timeLeft){
+            for (int i=0; i<5;i++){
+            Dust dust = Dust.NewDustDirect(Projectile.Center , Projectile.width, Projectile.height, ModContent.DustType<LuminiteBrightDust>(), Scale: 3);
+            dust.velocity = Projectile.velocity.RotatedByRandom(MathHelper.Pi*2)*Main.rand.NextFloat(0.1f,0.5f);
+            dust.noLight = false;
+            dust.noGravity = true;
             }
         }
-        public override void OnKill(int timeLeft)
+        public override Color? 	GetAlpha (Color lightColor)=>new Color(255,255,255,255)*Projectile.Opacity;
+        private SpriteBatchState state;
+        public override bool PreDraw(ref Color lightColor)
         {
-            for (int i = 0; i < 5; i++)
-            {
-                Dust dust = Dust.NewDustDirect(Projectile.Center, Projectile.width, Projectile.height, ModContent.DustType<LuminiteBrightDust>(), Scale: 3);
-                dust.velocity = Projectile.velocity.RotatedByRandom(MathHelper.Pi * 2) * Main.rand.NextFloat(0.1f, 0.5f);
-                dust.noLight = false;
-                dust.noGravity = true;
-            }
+            Projectile.DrawMagicPixelTrail(Vector2.Zero, 4f, 0.1f, colour.WithAlpha((byte)((Projectile.Opacity*0.5f)*255)), Color.Transparent);
+            return true;
         }
-        public override Color? GetAlpha(Color lightColor) => new Color(255, 255, 255, 255) * Projectile.Opacity;
+       
+       
     }
 }
