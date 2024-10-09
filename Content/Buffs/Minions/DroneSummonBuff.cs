e6c39808c284ad92f64c7a41e8f2f160d3015eb8
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-10-09 15:23:26 -0400

    Drone

---
+++ Content/Buffs/Minions/DroneSummonBuff.cs
@@ -0,0 +1,28 @@
+﻿using Macrocosm.Content.Projectiles.Friendly.Summon;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Macrocosm.Content.Buffs.Minions
+{
+    public class DroneSummonBuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            if (player.ownedProjectileCounts[ModContent.ProjectileType<DroneMinion>()] > 0)
+            {
+                player.buffTime[buffIndex] = 18000;
+            }
+            else
+            {
+                player.DelBuff(buffIndex);
+                buffIndex--;
+            }
+        }
+    }
+}
