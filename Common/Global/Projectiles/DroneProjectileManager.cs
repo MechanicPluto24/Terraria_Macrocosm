341c9f63012994855f0d5aada1bc6151738a0edd
George Feldy <georgefeldy@gmail.com>
2024-10-12 17:14:51 +0300

    merge refa 2

--- Common/Global/Projectiles/DroneProjectileManager.cs
+++ Common/Global/Projectiles/DroneProjectileManager.cs
@@ -1,33 +1,31 @@
-using Macrocosm.Common.Netcode;
-using System.IO;
+using Macrocosm.Content.Projectiles.Friendly.Summon;
 using Terraria;
 using Terraria.ModLoader;
-using Terraria.ModLoader.IO;
-using Macrocosm.Content.Projectiles.Friendly.Summon;
 namespace Macrocosm.Common.Global.Projectiles
 {
     public class DroneProjectileManager : ModSystem
     {
-        public override void PreUpdateProjectiles(){
+        public override void PreUpdateProjectiles()
+        {
             for (int i = 0; i < Main.maxProjectiles; i++)
             {
                 Projectile proj = Main.projectile[i];
                 if (proj.type == ModContent.ProjectileType<DroneMinion>() && proj.active)
                 {
-                float amountLesserThanI=0;
-                for (int j = 0; j < Main.maxProjectiles; j++)
-                {
-                Projectile projcheck = Main.projectile[j];
-                
-                if (projcheck.type == ModContent.ProjectileType<DroneMinion>() && projcheck.active)
+                    float amountLesserThanI = 0;
+                    for (int j = 0; j < Main.maxProjectiles; j++)
                     {
-                        if(projcheck.ai[2]>proj.ai[2])
+                        Projectile projcheck = Main.projectile[j];
+
+                        if (projcheck.type == ModContent.ProjectileType<DroneMinion>() && projcheck.active)
                         {
-                            amountLesserThanI+=1f;
+                            if (projcheck.ai[2] > proj.ai[2])
+                            {
+                                amountLesserThanI += 1f;
+                            }
                         }
                     }
-                }
-                proj.ai[1]=amountLesserThanI;
+                    proj.ai[1] = amountLesserThanI;
                 }
             }
         }
