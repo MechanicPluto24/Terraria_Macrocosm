bb6a3c3960c2b9a99509beea81b49a107fc03c81
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-10-14 19:50:53 -0400

    wrench claw

--- Content/Items/Weapons/Melee/ClawWrench.cs
+++ Content/Items/Weapons/Melee/ClawWrench.cs
@@ -2,6 +2,7 @@ using Macrocosm.Content.Projectiles.Friendly.Melee;
 using Macrocosm.Content.Rarities;
 using Microsoft.Xna.Framework;
 using Terraria;
+using Macrocosm.Common.Utils;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -12,7 +13,7 @@ namespace Macrocosm.Content.Items.Weapons.Melee
     {
         public override void SetStaticDefaults()
         {
-
+               ItemID.Sets.ItemsThatAllowRepeatedRightClick[Type] = true;
         }
 
         public override void SetDefaults()
@@ -24,7 +25,7 @@ namespace Macrocosm.Content.Items.Weapons.Melee
             Item.useTime = 20;
             Item.useAnimation = 20;
             Item.shoot = ModContent.ProjectileType<ClawWrenchProjectile>();
-            Item.shootSpeed = 2;
+            Item.shootSpeed = 45;
             Item.useStyle = ItemUseStyleID.Shoot;
             Item.DamageType = DamageClass.Melee;
             Item.noUseGraphic = true;
@@ -37,11 +38,13 @@ namespace Macrocosm.Content.Items.Weapons.Melee
         {
 
         }
+        public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Item.shoot] < 1;
+        public override bool AltFunctionUse(Player player) => true;
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            velocity.Y = 0;
-            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, Item.useTime);
+            
+            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, Item.useTime,player.AltFunction()?1f:0f);
             return false;
         }
     }
