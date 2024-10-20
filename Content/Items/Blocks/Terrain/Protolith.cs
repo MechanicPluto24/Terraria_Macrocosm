2f34ec04f0d82a41c2036c3a5283c8f57d080496
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-10-20 17:37:53 -0400

    gen

--- Content/Items/Blocks/Terrain/Protolith.cs
+++ Content/Items/Blocks/Terrain/Protolith.cs
@@ -1,6 +1,7 @@
 ﻿using Macrocosm.Content.Items.Walls;
 using Terraria;
 using Terraria.ModLoader;
+using Terraria.ID;
 
 namespace Macrocosm.Content.Items.Blocks.Terrain
 {
@@ -21,6 +22,43 @@ namespace Macrocosm.Content.Items.Blocks.Terrain
             CreateRecipe()
                 .AddIngredient<ProtolithWall>(4)
                 .Register();
+
+            Recipe.Create(ItemID.SolarBrick, 1)
+                .AddIngredient(Type, 10)
+                .AddIngredient(ItemID.FragmentSolar, 1)
+                .Register();
+            Recipe.Create(ItemID.LunarBlockSolar, 1)
+                .AddIngredient(Type, 5)
+                .AddIngredient(ItemID.FragmentSolar, 1)
+                .Register();
+
+            Recipe.Create(ItemID.VortexBrick, 1)
+                .AddIngredient(Type, 10)
+                .AddIngredient(ItemID.FragmentVortex, 1)
+                .Register();
+            Recipe.Create(ItemID.LunarBlockVortex, 1)
+                .AddIngredient(Type, 5)
+                .AddIngredient(ItemID.FragmentVortex, 1)
+                .Register();
+
+            Recipe.Create(ItemID.NebulaBrick, 1)
+                .AddIngredient(Type, 10)
+                .AddIngredient(ItemID.FragmentNebula, 1)
+                .Register();
+            Recipe.Create(ItemID.LunarBlockNebula, 1)
+                .AddIngredient(Type, 5)
+                .AddIngredient(ItemID.FragmentNebula, 1)
+                .Register();
+
+            Recipe.Create(ItemID.StardustBrick, 1)
+                .AddIngredient(Type, 10)
+                .AddIngredient(ItemID.FragmentStardust, 1)
+                .Register();
+            Recipe.Create(ItemID.LunarBlockStardust, 1)
+                .AddIngredient(Type, 5)
+                .AddIngredient(ItemID.FragmentStardust, 1)
+                .Register();
+            
         }
     }
 }
