a5052a280fd3645bea4a96ae556c9970f1c8204c
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-10-31 07:43:54 -0400

    CHEESE WALL AND PLATFORM

---
+++ Content/Items/Walls/CheeseWall.cs
@@ -0,0 +1,31 @@
+using Macrocosm.Content.Items.Blocks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Macrocosm.Content.Items.Walls
+{
+    public class CheeseWall : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 400;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableWall(ModContent.WallType<Tiles.Walls.CheeseWall>());
+            Item.width = 24;
+            Item.height = 24;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(4)
+                .AddIngredient<CheeseBlock>()
+                .AddTile(TileID.WorkBenches)
+                .Register();
+        }
+    }
+
+}
