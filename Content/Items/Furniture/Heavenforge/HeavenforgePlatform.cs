14c2995381527591f25cfea87d160d24918f8eed
George Feldy <georgefeldy@gmail.com>
2024-10-31 02:58:11 +0200

    luminite platforms

---
+++ Content/Items/Furniture/Heavenforge/HeavenforgePlatform.cs
@@ -0,0 +1,29 @@
+using Macrocosm.Common.Enums;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Macrocosm.Content.Items.Furniture.Heavenforge
+{
+    public class HeavenforgePlatform : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 200;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Furniture.Luminite.LuminitePlatform>(), (int)LuminiteStyle.Heavenforge);
+            Item.width = 24;
+            Item.height = 16;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(2)
+                .AddIngredient(ItemID.HeavenforgeBrick)
+                .Register();
+        }
+    }
+}
