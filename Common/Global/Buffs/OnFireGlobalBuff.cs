55fb836be661049b243d7f61303c4cd4d0db7e69
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-10-31 21:02:00 -0400

    Short fix

--- Common/Global/Buffs/OnFireGlobalBuff.cs
+++ Common/Global/Buffs/OnFireGlobalBuff.cs
@@ -1,5 +1,6 @@
 ﻿using Macrocosm.Content.Debuffs;
 using Terraria;
+using SubworldLibrary;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -9,7 +10,7 @@ namespace Macrocosm.Common.Global.Buffs
     {
         public override void Update(int type, NPC npc, ref int buffIndex)
         {
-            if (type is BuffID.OnFire or BuffID.OnFire3)
+            if (type is BuffID.OnFire or BuffID.OnFire3&&SubworldSystem.AnyActive<Macrocosm>())
             {
                 int time = npc.buffTime[buffIndex];
 
@@ -25,7 +26,7 @@ namespace Macrocosm.Common.Global.Buffs
 
         public override void Update(int type, Player player, ref int buffIndex)
         {
-            if (type is BuffID.OnFire or BuffID.OnFire3)
+            if (type is BuffID.OnFire or BuffID.OnFire3&&SubworldSystem.AnyActive<Macrocosm>())
             {
                 int time = player.buffTime[buffIndex];
 
