fface83566d511c5b5074142feaf648ea85ab2e7
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-11-01 18:37:09 -0400

    Fixes

---
+++ Common/Global/Items/BloodTearGlobalItem.cs
@@ -0,0 +1,23 @@
+using Macrocosm.Common.Sets;
+using SubworldLibrary;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Macrocosm.Common.Global.Items
+{
+    public class BloodTearGlobalItem : GlobalItem
+    {
+        public override bool CanUseItem(Item item, Player player)
+        {
+            if (SubworldSystem.AnyActive<Macrocosm>()) // TODO: Add check, if airless environment
+            {
+                if (item.type==ItemID.BloodMoonStarter)
+                    return false;
+                
+            }
+
+            return true;
+        }
+    }
+}
