55cf5a7a7c442ac897c4da8b992f527c2397da0a
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-11-21 19:06:01 -0500

    Kerosene burner, needs vibration fix

---
+++ Content/Machines/KeroseneGenerator.cs
@@ -0,0 +1,161 @@
+﻿using Macrocosm.Common.Bases.Tiles;
+using Macrocosm.Common.Drawing;
+using Macrocosm.Common.Drawing.Particles;
+using Macrocosm.Common.Subworlds;
+using Macrocosm.Common.Systems.Power;
+using Macrocosm.Common.Systems.UI;
+using Macrocosm.Common.Utils;
+using Macrocosm.Content.Particles;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Macrocosm.Content.Machines
+{
+    public class KeroseneGenerator : MachineTile
+    {
+        public override short Width => 6;
+        public override short Height => 4;
+        public override MachineTE MachineTE => ModContent.GetInstance<KeroseneGeneratorTE>();
+        float vibrationX;
+        float vibrationY;
+        public override void SetStaticDefaults()
+        {
+            Main.tileLighted[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileWaterDeath[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style6x3);
+            TileObjectData.newTile.Width = Width;
+            TileObjectData.newTile.Height = Height;
+
+            TileObjectData.newTile.CoordinateHeights = [18, 18, 18,18];
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinatePadding = 2;
+
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.StyleLineSkip = 6;
+            TileObjectData.newTile.StyleWrapLimit = 6;
+
+            TileObjectData.newTile.DrawYOffset = 2;
+
+            TileObjectData.newTile.Origin = new Point16(0, Height - 1);
+            TileObjectData.newTile.AnchorTop = new AnchorData();
+            TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop, Width, 0);
+
+            TileObjectData.newTile.AnchorInvalidTiles =
+            [
+                TileID.MagicalIceBlock,
+                TileID.Boulder,
+                TileID.BouncyBoulder,
+                TileID.LifeCrystalBoulder,
+                TileID.RollingCactus
+            ];
+
+            TileObjectData.newTile.HookPostPlaceMyPlayer = new PlacementHook(MachineTE.Hook_AfterPlacement, -1, 0, false);
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+
+            TileObjectData.addTile(Type);
+
+            DustType = -1;
+
+            AddMapEntry(new Color(145, 80, 20), CreateMapEntryName());
+        }
+
+
+
+        
+
+       
+        public override void NearbyEffects(int 	i,int j,bool closer){
+            vibrationY=Main.rand.NextFloat(-4f,5f);
+        }
+        public override void DrawEffects(int i, int j, SpriteBatch spriteBatch, ref TileDrawInfo drawData)
+        {
+            if (Main.gamePaused)
+                return;
+
+            Tile tile = Main.tile[i, j];
+            int tileOffsetX = tile.TileFrameX % (Width * 18) / 18;
+            int tileOffsetY = tile.TileFrameY % (Height * 18) / 18;
+            if (Utility.TryGetTileEntityAs(i, j, out KeroseneGeneratorTE keroseneGenerator)){
+            if (keroseneGenerator.PoweredOn)
+            {
+                // Exhaust position - spawn smoke
+                if (tileOffsetX == 0 && tileOffsetY == 0)
+                {
+                    {
+                        float atmoDensity = (0.3f + 0.7f * MacrocosmSubworld.CurrentAtmosphericDensity);
+                        for (int k = 0; k < 4; k++)
+                        {
+                            if (Main.rand.NextBool(2))
+                                continue;
+
+                            Smoke smoke = Particle.Create<Smoke>((p) =>
+                            {
+                                p.Position = new Vector2(i+2, j) * 16f + new Vector2(k % 2 == 0 ? 4 : 13, 18f);
+                                p.Velocity = new Vector2(0, -0.7f).RotatedByRandom(MathHelper.Pi / 16) * atmoDensity;
+                                p.Scale = new(0.1f);
+                                p.Rotation = 0f;
+                                p.Color = (new Color(80, 80, 80) * Main.rand.NextFloat(0.75f, 1f)).WithAlpha(215);
+                                p.FadeIn = true;
+                                p.Opacity = 0f;
+                                p.ScaleVelocity = new(0.0075f);
+                                p.WindFactor = 0.01f;
+                            });
+                        }
+                    }
+                }
+            }
+            }
+
+            
+        }
+        public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+            var vibration = ModContent.Request<Texture2D>(Texture + "VibrationPart");
+            if (Utility.TryGetTileEntityAs(i, j, out KeroseneGeneratorTE keroseneBurner)){
+                if (keroseneBurner.PoweredOn){
+                    Utility.DrawTileExtraTextureWithOffset(i, j, spriteBatch, vibration,i2:vibrationX,j2:vibrationY);
+                }
+                else{
+                    Utility.DrawTileExtraTexture(i, j, spriteBatch, vibration);
+                }
+            }
+        }
+        public override bool RightClick(int i, int j)
+        {
+            Main.mouseRightRelease = false;
+            Utility.UICloseOthers();
+
+            
+                if (Utility.TryGetTileEntityAs(i, j, out KeroseneGeneratorTE keroseneGenerator))
+                    UISystem.ShowMachineUI(keroseneGenerator, new KeroseneGeneratorUI());
+            
+
+            return true;
+        }
+
+        public override void MouseOver(int i, int j)
+        {
+            Player player = Main.LocalPlayer;
+
+            if (!player.mouseInterface)
+            {
+                player.noThrow = 2;
+                Point16 origin = Utility.GetMultitileTopLeft(i, j);
+                Main.LocalPlayer.cursorItemIconEnabled = true;
+                player.cursorItemIconID = TileLoader.GetItemDropFromTypeAndStyle(Type, TileObjectData.GetTileStyle(Main.tile[i, j]));
+                
+            }
+        }
+
+    }
+}
---
+++ Content/Machines/KeroseneGeneratorTE.cs
@@ -0,0 +1,166 @@
+﻿using Macrocosm.Common.DataStructures;
+using Macrocosm.Common.Enums;
+using Macrocosm.Common.Sets;
+using Macrocosm.Common.Storage;
+using Macrocosm.Common.Systems.Power;
+using Microsoft.Xna.Framework;
+using System.IO;
+using Terraria;
+using Macrocosm.Content.Items.LiquidContainers;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace Macrocosm.Content.Machines
+{
+    public class KeroseneGeneratorTE : GeneratorTE, IInventoryOwner
+    {
+        public override MachineTile MachineTile => ModContent.GetInstance<KeroseneGenerator>();
+
+        /// <summary> The hull heat progress, 0 to 1, increases when burning and decreases otherwise. </summary>
+        public float HullHeatProgress
+        {
+            get => hullHeatProgress;
+            set => hullHeatProgress = MathHelper.Clamp(value, 0f, 1f);
+        }
+        protected float hullHeatProgress;
+
+        /// <summary> The hull heat, in degrees Celsius </summary>
+        public float HullHeat => 1200f * HullHeatProgress;
+
+        /// <summary> The burning progress of the <see cref="ConsumedItem"/> </summary>
+        public float BurnProgress => ConsumedItem.type != ItemID.None ? 1f - (float)burnTimer / ItemSets.FuelData[ConsumedItem.type].ConsumptionRate : 0f;
+        protected int burnTimer;
+
+        /// <summary> The max power out that the Burner can have, at max <see cref="HullHeatProgress"/></summary>
+        public float MaxPower => 10f;
+
+        /// <summary> The rate at which <see cref="HullHeatProgress"/> changes. </summary>
+        public float HullHeatRate => 0.0001f;
+
+        /// <summary> The item currently being burned. </summary>
+        public Item ConsumedItem { get; set; } = new(ItemID.None);
+        public override bool PoweredOn=>BurningOil();
+        public bool BurningOil(){
+            foreach (Item item in Inventory)
+                    {
+                        FuelData fuelData = ItemSets.FuelData[item.type];
+                        if (fuelData.Valid&&item.type==ModContent.ItemType<RocketFuelCanister>())
+                        {
+                            return true;
+                        }
+                    }
+            return false;
+        }
+
+        public Inventory Inventory { get; set; }
+        protected virtual int InventorySize => 1;
+        public Vector2 InventoryItemDropLocation => Position.ToVector2() * 16 + new Vector2(MachineTile.Width, MachineTile.Height) * 16 / 2;
+
+        public override void OnFirstUpdate()
+        {
+            // Create new inventory if none found on world load
+            Inventory ??= new(InventorySize, this);
+
+            // Assign inventory owner if the inventory was found on load
+            // IInvetoryOwner does not work well with TileEntities >:(
+            if (Inventory.Owner is null)
+                Inventory.Owner = this;
+        }
+
+        public override void MachineUpdate()
+        {
+            if (PoweredOn)
+            {
+                if (ConsumedItem.type != ItemID.None)
+                {
+                    FuelData fuelData = ItemSets.FuelData[ConsumedItem.type];
+                    HullHeatProgress += HullHeatRate * (float)fuelData.Potency;
+
+                    if (burnTimer++ >= fuelData.ConsumptionRate)
+                    {
+                        burnTimer = 0;
+                        ConsumedItem.TurnToAir(fullReset: true);
+                    }
+                }
+                else
+                {
+                    burnTimer = 0;
+                    bool fuelFound = false;
+
+                    foreach (Item item in Inventory)
+                    {
+                        FuelData fuelData = ItemSets.FuelData[item.type];
+                        if (fuelData.Valid&&item.type==ModContent.ItemType<RocketFuelCanister>())
+                        {
+                            ConsumedItem = new(item.type, stack: 1);
+                            HullHeatProgress += HullHeatRate * (float)fuelData.Potency;
+
+                            item.stack--;
+                            if (item.stack < 0)
+                                item.TurnToAir();
+
+                            fuelFound = true;
+                            break;
+                        }
+                    }
+
+                    if (!fuelFound)
+                    {
+                        GeneratedPower = 0;
+                        TurnOff();
+                    }
+                }
+            }
+            else
+            {
+                HullHeatProgress -= HullHeatRate;
+            }
+
+            GeneratedPower = HullHeatProgress * MaxPower;
+        }
+      
+
+        public override void NetSend(BinaryWriter writer)
+        {
+            Inventory ??= new(InventorySize, this);
+            TagIO.Write(Inventory.SerializeData(), writer);
+
+            ItemIO.Send(ConsumedItem, writer);
+
+            writer.Write(hullHeatProgress);
+        }
+
+        public override void NetReceive(BinaryReader reader)
+        {
+            TagCompound tag = TagIO.Read(reader);
+            Inventory = Inventory.DeserializeData(tag);
+
+            ConsumedItem = ItemIO.Receive(reader);
+
+            hullHeatProgress = reader.ReadSingle();
+        }
+
+        public override void SaveData(TagCompound tag)
+        {
+            tag[nameof(Inventory)] = Inventory;
+
+            tag[nameof(ConsumedItem)] = ItemIO.Save(ConsumedItem);
+
+            if(hullHeatProgress > 0f)
+                tag[nameof(hullHeatProgress)] = hullHeatProgress;
+        }
+
+        public override void LoadData(TagCompound tag)
+        {
+            if (tag.ContainsKey(nameof(Inventory)))
+                Inventory = tag.Get<Inventory>(nameof(Inventory));
+
+            if (tag.ContainsKey(nameof(ConsumedItem)))
+                ItemIO.Load(ConsumedItem, tag.GetCompound(nameof(ConsumedItem)));
+
+            if (tag.ContainsKey(nameof(hullHeatProgress)))
+                hullHeatProgress = tag.GetFloat(nameof(hullHeatProgress));
+        }
+    }
+}
