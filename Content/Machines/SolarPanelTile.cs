ca863b2ed8cdaf215e002375dff2f76360a2956a
clyder8 <109995796+clyder8@users.noreply.github.com>
2024-12-15 18:24:58 -0500

    I love giving other people work. Incomplete solar panel unsure why it isn't working because I suck with machines (I need an enemy or weapon to do lol)

---
+++ Content/Machines/SolarPanelTile.cs
@@ -0,0 +1,37 @@
+﻿using Macrocosm.Common.Systems.Power;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Macrocosm.Content.Dusts;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Macrocosm.Content.Machines
+{
+    public class SolarPanelTile : MachineTile
+    {
+        public override short Width => 1;
+        public override short Height => 1;
+        public override MachineTE MachineTE => ModContent.GetInstance<SolarPanelTileTE>();
+
+        public override void SetStaticDefaults()
+        {
+            TileObjectData.newTile.HookPostPlaceMyPlayer = new PlacementHook(MachineTE.Hook_AfterPlacement, -1, 0, false);
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+            TileObjectData.newTile.Width = 1;
+            TileObjectData.newTile.Height = 1;
+            TileObjectData.newTile.Origin = new Point16(0, 0);
+            
+            Main.tileFrameImportant[Type] = true;
+            Main.tileSolid[Type] = true;
+            Main.tileBlockLight[Type] = false;
+            DustType = ModContent.DustType<IndustrialPlatingDust>();
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(100, 100, 200), CreateMapEntryName());
+        }
+    }
+}
