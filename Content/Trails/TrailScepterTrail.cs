41a81dc8644b02c4f7625c894e238149310c398a
clyder8 <109995796+clyder8@users.noreply.github.com>
2025-01-24 09:38:40 -0500

    Trail scepter improvements

---
+++ Content/Trails/TrailScepterTrail.cs
@@ -0,0 +1,32 @@
+﻿using Macrocosm.Common.Drawing.Trails;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Graphics.Shaders;
+using Terraria.ModLoader;
+
+namespace Macrocosm.Content.Trails
+{
+    public class TrailScepterTrail : VertexTrail
+    {
+
+        public override MiscShaderData TrailShader => new MiscShaderData(Main.VertexPixelShaderRef, "MagicMissile")
+            .UseProjectionMatrix(doUse: true)
+            .UseSaturation(Saturation)
+            .UseImage0(ModContent.Request<Texture2D>(Macrocosm.TextureEffectsPath + "FadeOutTrail"))
+            .UseImage1(ModContent.Request<Texture2D>(Macrocosm.TextureEffectsPath + "RocketExhaustTrail1"))
+            .UseImage2(ModContent.Request<Texture2D>(Macrocosm.TextureEffectsPath + "Spark7"));
+
+        public override float Saturation => -2f;
+
+        public override Color TrailColors(float progressOnStrip)
+        {
+            return Color.Lerp(new Color(255, 255, 255, 0) * 1f, new Color(0, 217, 102, 0) * 1f, progressOnStrip * 2f / progressOnStrip);
+        }
+
+        public override float TrailWidths(float progressOnStrip)
+        {
+            return MathHelper.Lerp(20f, 7f, progressOnStrip);
+        }
+    }
+}
