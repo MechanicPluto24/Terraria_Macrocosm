6d25b9bc90b0ef424ca8a9b2a7911c25106f1e6b
clyder8 <109995796+clyder8@users.noreply.github.com>
2025-01-27 11:24:45 -0500

    Clouds and ilmenite and frig stuff feldy uh please do the opacity

---
+++ Content/Clouds/Pollution/PollutionClouds.cs
@@ -0,0 +1,64 @@
+﻿using Macrocosm.Content.Biomes;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace Macrocosm.Content.Clouds.Pollution
+{
+	// This class showcases advanced usage of a modded cloud. A ModCloud class is only needed for clouds with custom logic.
+	// Typical clouds can be autoloaded automatically from any "Clouds" folder or manually loaded via CloudLoader.AddCloudFromTexture method for greater control.
+	public abstract class BasePollutionCloud : ModCloud
+    {
+		public override bool RareCloud => false;
+
+		public override float SpawnChance() {
+			
+			if (!Main.gameMenu && Main.LocalPlayer.InModBiome<PollutionBiome>()) {
+				return 25f;
+			}
+
+			return 0f;
+		}
+
+		public override void OnSpawn(Cloud cloud) {
+		}
+		int despawnTimer=0;
+		public override bool Draw(SpriteBatch spriteBatch, Cloud cloud, int cloudIndex, ref DrawData drawData) {
+			if(!Main.LocalPlayer.InModBiome<PollutionBiome>())
+			{
+				despawnTimer++;
+				drawData.color*=0.9f;
+				if(despawnTimer>60)
+					cloud.active=false;
+			}
+			else
+				despawnTimer=0;
+			return true;
+		}
+	}
+	public class PollutionCloud1 : BasePollutionCloud
+    {
+	}
+	public class PollutionCloud2 : BasePollutionCloud
+    {
+	}
+	public class PollutionCloud3 : BasePollutionCloud
+    {
+	}
+	public class PollutionCloud4 : BasePollutionCloud
+    {
+	}
+	public class PollutionCloud5 : BasePollutionCloud
+    {
+	}
+	public class PollutionCloud6 : BasePollutionCloud
+    {
+	}
+	public class PollutionCloud7 : BasePollutionCloud
+    {
+	}
+	public class PollutionCloud8 : BasePollutionCloud
+    {
+	}
+}
