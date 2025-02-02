779205c0ae797d5a52cb12b9c1fb01e4b463cf62
clyder8 <109995796+clyder8@users.noreply.github.com>
2025-02-02 13:15:55 -0500

    Resprites, thanks pil

--- Content/NPCs/Enemies/Pollution/TarBlob.cs
+++ Content/NPCs/Enemies/Pollution/TarBlob.cs
@@ -1,6 +1,12 @@
+using Macrocosm.Common.DataStructures;
+using Macrocosm.Common.Players;
+using Macrocosm.Common.Sets;
 using Macrocosm.Common.Utils;
 using Macrocosm.Content.Biomes;
 using Macrocosm.Content.Dusts;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -11,7 +17,7 @@ namespace Macrocosm.Content.NPCs.Enemies.Pollution
     {
         public override void SetStaticDefaults()
         {
-            Main.npcFrameCount[NPC.type] = Main.npcFrameCount[NPCID.BlueSlime];
+            Main.npcFrameCount[NPC.type] = 4;
 
             NPC.ApplyImmunity
             (
@@ -36,12 +42,28 @@ namespace Macrocosm.Content.NPCs.Enemies.Pollution
             NPC.value = 60f;
             NPC.knockBackResist = 0.5f;
             NPC.aiStyle = 1;
-            NPC.alpha = 30;
+            NPC.alpha=10;
             AIType = NPCID.BlueSlime;
-            AnimationType = NPCID.BlueSlime;
             Banner = Item.NPCtoBanner(NPCID.BlueSlime);
             BannerItem = Item.BannerToItem(Banner);
             SpawnModBiomes = [ModContent.GetInstance<PollutionBiome>().Type];
+        }
+        public override void FindFrame(int frameHeight)
+        {
+            int frameSpeed = 8;
+
+            NPC.frameCounter++;
+
+            if (NPC.frameCounter >= frameSpeed)
+            {
+                NPC.frameCounter = 0;
+                NPC.frame.Y += frameHeight;
+
+                if (NPC.frame.Y >= Main.npcFrameCount[Type] * frameHeight)
+                {
+                    NPC.frame.Y = 0;
+                }
+            }
         }
 
         public override void OnHitPlayer(Player target, Player.HurtInfo hurtInfo)
@@ -58,17 +80,8 @@ namespace Macrocosm.Content.NPCs.Enemies.Pollution
 
         }
 
-        private int timer;
         public override bool PreAI()
-        {
-            if ((timer++ % 5) == 0)
-            {
-                Dust dust = Dust.NewDustPerfect(NPC.Center + Main.rand.NextVector2Circular(20, 10), ModContent.DustType<TarDust>());
-                dust.velocity.X = Main.rand.NextFloat(-1f, 1f);
-                dust.velocity.Y = 0.9f + Main.rand.Next(-50, 51) * 0.01f;
-                dust.scale *=  Main.rand.Next(-10, 11) * 0.1f;
-            }
-
+        { 
             return true;
         }
 
@@ -76,12 +89,14 @@ namespace Macrocosm.Content.NPCs.Enemies.Pollution
         {
             for (int i = 0; i < 5; i++)
             {
-                int dustIndex = Dust.NewDust(NPC.position, NPC.width, NPC.height, ModContent.DustType<TarDust>());
+                int dustIndex = Dust.NewDust(NPC.position, NPC.width, NPC.height, ModContent.DustType<CoalDust>());
                 Dust dust = Main.dust[dustIndex];
                 dust.velocity.X *= dust.velocity.X * 1.25f * hit.HitDirection + Main.rand.Next(0, 100) * 0.015f;
                 dust.velocity.Y *= dust.velocity.Y * 0.25f + Main.rand.Next(-50, 51) * 0.01f;
                 dust.scale *= 1f + Main.rand.Next(-30, 31) * 0.01f;
             }
         }
+
+
     }
 }
