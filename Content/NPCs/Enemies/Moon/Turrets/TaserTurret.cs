5229ec8760796c84e17c3089f943ccb45e06c045
clyder8 <109995796+clyder8@users.noreply.github.com>
2025-03-30 15:39:19 -0400

    Sentry, also fixed turrets (Why were they broken?)

--- Content/NPCs/Enemies/Moon/Turrets/TaserTurret.cs
+++ Content/NPCs/Enemies/Moon/Turrets/TaserTurret.cs
@@ -1,28 +1,20 @@
 using Macrocosm.Common.Sets;
 using Macrocosm.Content.Dusts;
 using Macrocosm.Content.Items.Tech;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using ReLogic.Content;
 using Terraria;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria.ModLoader;
+using ReLogic.Content;
+using Macrocosm.Content.Projectiles.Hostile;
 
 namespace Macrocosm.Content.NPCs.Enemies.Moon.Turrets
 {
     public class TaserTurret : ModNPC
     {
         private static Asset<Texture2D> turretTexture;
-        public override string Texture => "Macrocosm/Content/NPCs/Enemies/Moon/Turrets/Turret_Base";
-
-        public int AI_Timer
-        {
-            get => (int)NPC.ai[0];
-            set => NPC.ai[0] = value;
-        }
-
-        public Vector2 turretHeight = new(0, -22);
 
         public override void SetStaticDefaults()
         {
@@ -31,7 +23,7 @@ namespace Macrocosm.Content.NPCs.Enemies.Moon.Turrets
             NPCSets.MoonNPC[Type] = true;
             NPCSets.DropsMoonstone[Type] = false;
         }
-
+        public override string Texture => "Macrocosm/Content/NPCs/Enemies/Moon/Turrets/Turret_Base";
         public override void SetDefaults()
         {
             NPC.width = 38;
@@ -46,55 +38,59 @@ namespace Macrocosm.Content.NPCs.Enemies.Moon.Turrets
             NPC.aiStyle = -1;
         }
 
-        public override float SpawnChance(NPCSpawnInfo spawnInfo) => 0f;
-
-        public override void AI()
-        {
-            NPC.TargetClosest(faceTarget: true);
-            Player player = Main.player[NPC.target];
-
-            if (NPC.ai[0] == 0f)
-            {
-                Vector2 turningVector = (player.Center - NPC.Center).SafeNormalize(Vector2.UnitX);
-                NPC.rotation = (new Vector2(5, 0).RotatedBy(NPC.rotation) + (turningVector * 0.3f)).ToRotation();
-
-                bool clearLineOfSight = Collision.CanHitLine(NPC.position, NPC.width, NPC.height, player.position, player.width, player.height);
-                if (NPC.HasPlayerTarget && clearLineOfSight)
-                {
-                    if ((NPC.direction >= 0 && player.Center.X > NPC.Center.X) || (NPC.direction < 0 && player.Center.X <= NPC.Center.X))
-                        AI_Timer++;
-
-                    if (AI_Timer == 120)
-                    {
-                        Projectile.NewProjectileDirect(NPC.GetSource_FromAI(), NPC.Center + turretHeight + new Vector2(15, 0).RotatedBy(NPC.rotation), new Vector2(15f, 0).RotatedBy(NPC.rotation), ModContent.ProjectileType<TurretTaserProjectile>(), 50, 1f, Main.myPlayer, ai1: NPC.whoAmI);
-                        NPC.ai[0] = 1f;
-                        AI_Timer = 0;
-                    }
-                }
-                else
-                {
-                    AI_Timer = 0;
-                }
-            }
-        }
-
         public override void OnHitPlayer(Player target, Player.HurtInfo hurtInfo)
         {
         }
+        public Vector2 TurretHeight=new Vector2(0,-22);
+        private float TurretRotation =0f;  
+        int AI_Timer=0;
+        
+        public override void AI()
+        {
+            NPC.TargetClosest(faceTarget:true);
+            Player player = Main.player[NPC.target];
+           
+            if( NPC.ai[0]==0f){
+            Vector2 turningVector = (player.Center - NPC.Center).SafeNormalize(Vector2.UnitX);
+            TurretRotation = (new Vector2(5,0).RotatedBy(TurretRotation) + (turningVector * 0.3f)).ToRotation();
 
+            bool clearLineOfSight = Collision.CanHitLine(NPC.position, NPC.width, NPC.height, player.position, player.width, player.height);
+            if (NPC.HasPlayerTarget && clearLineOfSight)
+            {
+                if((NPC.direction>=0&&player.Center.X>NPC.Center.X)||(NPC.direction<0&&player.Center.X<=NPC.Center.X))
+                    AI_Timer++;
+                if(AI_Timer==120)
+                {
+                    Projectile.NewProjectileDirect(NPC.GetSource_FromAI(), NPC.Center+TurretHeight+new Vector2(15,0).RotatedBy(TurretRotation), new Vector2(15f,0).RotatedBy(TurretRotation), ModContent.ProjectileType<TurretTaserProjectile>(), 50, 1f, Main.myPlayer, ai1 : NPC.whoAmI);
+                    NPC.ai[0]=1f;
+                    AI_Timer=0;
+                }
+                
+            }
+            else{
+                AI_Timer=0;
+            }
+            }
+            
+        }
         public override void PostDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
         {
             turretTexture ??= ModContent.Request<Texture2D>("Macrocosm/Content/NPCs/Enemies/Moon/Turrets/Turret_Taser_Top");
-            Rectangle frameOfGun = turretTexture.Frame(1, 2, 0, NPC.ai[0] == 0f ? 0 : 1);
-            spriteBatch.Draw(turretTexture.Value, NPC.Center + turretHeight - Main.screenPosition, frameOfGun, drawColor, NPC.rotation, frameOfGun.Size() / 2, NPC.scale, NPC.direction > 0 ? SpriteEffects.None : SpriteEffects.FlipVertically, 0f);
+            Rectangle frameOfGun = turretTexture.Frame(1, 2, 0, NPC.ai[0]==0f ? 0 : 1);
+            spriteBatch.Draw(turretTexture.Value, NPC.Center + TurretHeight - Main.screenPosition, frameOfGun, drawColor, TurretRotation, frameOfGun.Size() / 2, NPC.scale,  NPC.direction > 0 ? SpriteEffects.None : SpriteEffects.FlipVertically, 0f);
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return 0f;
         }
 
         public override void ModifyNPCLoot(NPCLoot loot)
         {
-            loot.Add(ItemDropRule.Common(ModContent.ItemType<PrintedCircuitBoard>(), 5));
-            loot.Add(ItemDropRule.Common(ModContent.ItemType<Motor>(), 5));
-            loot.Add(ItemDropRule.Common(ModContent.ItemType<Gear>(), 5));
-            loot.Add(ItemDropRule.Common(ModContent.ItemType<Battery>(), 5));
+            loot.Add(ItemDropRule.Common(ModContent.ItemType<PrintedCircuitBoard>(), 2));
+            loot.Add(ItemDropRule.Common(ModContent.ItemType<Motor>(), 2));
+            loot.Add(ItemDropRule.Common(ModContent.ItemType<Gear>(), 2));
+            loot.Add(ItemDropRule.Common(ModContent.ItemType<Battery>(), 2));
         }
 
         public override void HitEffect(NPC.HitInfo hit)
---
+++ Content/Projectiles/Friendly/Summon/Sentries/MoonChampionSentry.cs
@@ -0,0 +1,149 @@
+using System.Collections.Generic;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Macrocosm.Content.Projectiles.Friendly.Ranged;
+
+namespace Macrocosm.Content.Projectiles.Friendly.Summon.Sentries
+{
+    public class MoonChampionSentry : ModProjectile
+    {
+        private static Asset<Texture2D> turretTexture;
+        private static Asset<Texture2D> backTexture;
+
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.MinionSacrificable[Projectile.type] = true;
+            ProjectileID.Sets.CultistIsResistantTo[Projectile.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 40;
+            Projectile.height = 46;
+            Projectile.tileCollide = true;
+            Projectile.sentry = true;
+            Projectile.timeLeft = Projectile.SentryLifeTime;
+
+            Projectile.friendly = true;
+            Projectile.ignoreWater = true;
+            Projectile.DamageType = DamageClass.Summon;
+            Projectile.penetrate = -1;
+        }
+        public override bool? CanDamage() => false;
+        private NPC target;
+        bool init=false;
+        private float TurretRotation =0f;  
+        int Timer=0;
+        float offset=0f;
+        public override void AI()
+        {
+            Player owner = Main.player[Projectile.owner];
+            if(!init){
+                init=true;
+                Projectile.spriteDirection=owner.Center.X<Projectile.Center.X ? -1:1;
+            }
+            Projectile.velocity.Y += 1;
+            SearchForTargets(owner, out bool foundTarget, out float distanceFromTarget, out Vector2 targetCenter);
+            
+            if(foundTarget&&Projectile.spriteDirection == -1 ? targetCenter.X<Projectile.Center.X : targetCenter.X>Projectile.Center.X){
+                Vector2 turningVector = (targetCenter- Projectile.Center).SafeNormalize(Vector2.UnitX);
+                TurretRotation = (new Vector2(5,0).RotatedBy(TurretRotation) + (turningVector * 0.6f)).ToRotation();
+                Timer++;
+                if(Timer==60)
+                {
+                    Projectile p = Projectile.NewProjectileDirect(Projectile.GetSource_FromAI(), Projectile.Center+new Vector2(-14,0), new Vector2(16f,0).RotatedBy(TurretRotation), ModContent.ProjectileType<NWAMissile>(), Projectile.damage/2, 1f, Main.myPlayer);
+                    Timer=0;
+                    p.DamageType= DamageClass.Summon;
+                    offset=5f;
+                }
+            }
+            else
+            {
+                
+                TurretRotation = MathHelper.Lerp(TurretRotation,Projectile.spriteDirection == 1 ? 0 : MathHelper.Pi,0.08f);
+                Timer=0;        
+            }
+            offset*=0.9f;
+
+        }
+        private void SearchForTargets(Player owner, out bool foundTarget, out float distanceFromTarget, out Vector2 targetCenter)
+        {
+
+            distanceFromTarget = 700f;
+            targetCenter = Projectile.position;
+            foundTarget = false;
+
+            // This code is required if your minion weapon has the targeting feature
+            if (owner.HasMinionAttackTargetNPC)
+            {
+                NPC npc = Main.npc[owner.MinionAttackTargetNPC];
+                float between = Vector2.Distance(npc.Center, Projectile.Center);
+
+                // Reasonable distance away so it doesn't target across multiple screens
+                if (between < 2000f)
+                {
+                    distanceFromTarget = between;
+                    targetCenter = npc.Center;
+                    foundTarget = true;
+                }
+            }
+
+            if (!foundTarget && Projectile.alpha < 1)
+            {
+                // This code is required either way, used for finding a target
+                for (int i = 0; i < Main.maxNPCs; i++)
+                {
+                    NPC npc = Main.npc[i];
+
+                    if (npc.CanBeChasedBy()&&Vector2.Distance(npc.Center, Projectile.Center)<4000f)
+                    {
+                        float between = Vector2.Distance(npc.Center, Projectile.Center);
+                        bool closest = Vector2.Distance(Projectile.Center, targetCenter) > between;
+                        bool inRange = between < distanceFromTarget;
+                        bool lineOfSight = Collision.CanHitLine(Projectile.position, Projectile.width, Projectile.height, npc.position, npc.width, npc.height);
+                        // Additional check for this specific minion behavior, otherwise it will stop attacking once it dashed through an enemy while flying though tiles afterwards
+                        // The number depends on various parameters seen in the movement code below. Test different ones out until it works alright
+                        bool closeThroughWall = between < 100f;
+
+                        if (((closest && inRange) || !foundTarget) && (lineOfSight || closeThroughWall))
+                        {
+                            distanceFromTarget = between;
+                            targetCenter = npc.Center;
+                            foundTarget = true;
+                        }
+                    }
+                }
+            }
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            turretTexture ??= ModContent.Request<Texture2D>("Macrocosm/Content/Projectiles/Friendly/Summon/Sentries/MoonChampionSentryTurret");
+            backTexture ??= ModContent.Request<Texture2D>("Macrocosm/Content/Projectiles/Friendly/Summon/Sentries/MoonChampionSentryBack");
+            SpriteEffects effect = Projectile.spriteDirection == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+            SpriteEffects effect2 = Projectile.spriteDirection == -1 ? SpriteEffects.FlipVertically : SpriteEffects.None;
+
+            Main.EntitySpriteDraw(backTexture.Value, Projectile.Center- Main.screenPosition , null, lightColor, 0f, backTexture.Size() / 2, Projectile.scale, effect, 0f);
+            Main.EntitySpriteDraw(turretTexture.Value, Projectile.Center + new Vector2(0,-14)- Main.screenPosition+new Vector2(-offset,0).RotatedBy(TurretRotation) , null, lightColor, TurretRotation, turretTexture.Size() / 2, Projectile.scale, effect2, 0f);
+
+            return true;
+        }
+
+        public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac)
+        {
+            fallThrough = false;
+            return true;
+        }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            if (oldVelocity.Y > 0)
+                Projectile.velocity.Y = 0;
+            return false;
+        }
+       
+    }
+}
