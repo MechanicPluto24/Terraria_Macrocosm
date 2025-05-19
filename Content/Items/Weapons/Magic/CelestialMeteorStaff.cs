9e6e85f6b883a68b8a14d58d43acf9f76206c92c
clyder8 <109995796+clyder8@users.noreply.github.com>
2025-05-19 16:16:13 -0400

    Celestial meteor staff. Please fix stadust worm (It's so broken I can't tell what is causing the issue :dread:)

---
+++ Content/Items/Weapons/Magic/CelestialMeteorStaff.cs
@@ -0,0 +1,80 @@
+using Macrocosm.Content.Items.Bars;
+using Macrocosm.Content.Projectiles.Friendly.Magic;
+using Macrocosm.Content.Rarities;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Macrocosm.Content.Items.Weapons.Magic
+{
+    public class CelestialMeteorStaff : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.staff[Type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 500;
+            Item.DamageType = DamageClass.Magic;
+            Item.mana = 8;
+            Item.width = 50;
+            Item.height = 50;
+            Item.useTime = 30;
+            Item.useAnimation = 10;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.noMelee = true;
+            Item.knockBack = 5;
+            Item.value = 10000;
+            Item.rare = ModContent.RarityType<MoonRarity1>();
+            Item.UseSound = SoundID.Item20;
+            Item.autoReuse = true;
+            Item.shoot = ModContent.ProjectileType<CelestialMeteorStaffProjectileSolar>();
+            Item.shootSpeed = 10f;
+            Item.tileBoost = 50;
+        }
+
+        public override Vector2? HoldoutOrigin()
+            => new Vector2(0, 0);
+
+
+        public override void AddRecipes()
+        {
+
+        }
+        public int getMeteorType()
+        {
+            int integer= Main.rand.Next(0,4);
+            switch (integer)
+            {
+                case 0:
+                    return ModContent.ProjectileType<CelestialMeteorStaffProjectileSolar>();
+                    break;
+                case 1:
+                    return ModContent.ProjectileType<CelestialMeteorStaffProjectileVortex>();
+                    break;
+                case 2:
+                    return ModContent.ProjectileType<CelestialMeteorStaffProjectileNebula>();
+                    break;
+                case 3:
+                    return ModContent.ProjectileType<CelestialMeteorStaffProjectileStardust>();
+                    break;
+            }
+            return 0;
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockBack)
+        {  
+            Projectile.NewProjectileDirect(source, position, (new Vector2(Main.MouseWorld.X,player.Center.Y)-position).SafeNormalize(Vector2.UnitX)*25f, getMeteorType(), damage, knockBack, Main.LocalPlayer.whoAmI);
+            
+            return false;
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            position = new Vector2(Main.MouseWorld.X,player.Center.Y)+new Vector2(Main.rand.Next(-300,300),-1000);
+        }
+    }
+}
---
+++ Content/Projectiles/Friendly/Magic/NebulaRemnantProjectile.cs
@@ -0,0 +1,55 @@
+﻿using Macrocosm.Common.Subworlds;
+using Macrocosm.Common.Utils;
+using Macrocosm.Content.Dusts;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System;
+using Macrocosm.Common.Drawing.Particles;
+using Macrocosm.Content.Particles;
+
+namespace Macrocosm.Content.Projectiles.Friendly.Magic
+{
+    public class NebulaRemnantProjectile : ModProjectile
+    {
+       
+        public override void SetDefaults()
+        {
+            Projectile.width = 14;
+            Projectile.height = 22;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.timeLeft = 300;
+            Projectile.penetrate = -1;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 10;
+        }
+
+        public override void AI()
+        {
+            Projectile.velocity *=0.9f;
+            Projectile.rotation = 0f;
+
+            if (Projectile.timeLeft < 50)
+                Projectile.Opacity -= 0.02f;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            return false;
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = TextureAssets.Projectile[Type].Value;
+
+            Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition, null, Color.White*Projectile.Opacity, Projectile.rotation, texture.Size() / 2f, Projectile.scale, Projectile.spriteDirection == -1 ? SpriteEffects.FlipVertically : SpriteEffects.None);
+            Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition, null, Color.White*0.5f*Projectile.Opacity, Projectile.rotation, texture.Size() / 2f, (float)(Projectile.scale*((Math.Sin(Main.time*3f)*0.2f)+1.2f)), Projectile.spriteDirection == -1 ? SpriteEffects.FlipVertically : SpriteEffects.None);
+
+            return false;
+        }
+       
+    }
+}
