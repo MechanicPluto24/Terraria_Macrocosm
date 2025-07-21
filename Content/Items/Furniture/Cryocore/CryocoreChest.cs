4ee99d6ed4d6d09ea2a206aefd012cfa66c852c4
clyder8 <109995796+clyder8@users.noreply.github.com>
2025-07-21 14:15:54 -0400

    Syntax Error fix

--- Content/Items/Furniture/Cryocore/CryocoreChest.cs
+++ Content/Items/Furniture/Cryocore/CryocoreChest.cs
@@ -15,7 +15,7 @@ namespace Macrocosm.Content.Items.Furniture.Cryocore
 
         public override void SetDefaults()
         {
-            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Furniture.Luminite.LuminiteChest>(), (int)LuminiteStyle.Cryocore*2aa);
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Furniture.Luminite.LuminiteChest>(), (int)LuminiteStyle.Cryocore*2);
             Item.width = 32;
             Item.height = 24;
             Item.value = 150;
