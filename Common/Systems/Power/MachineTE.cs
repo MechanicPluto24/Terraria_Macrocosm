a20662969d0901f8274c871c745362122a5b7b6b
GeorgeFeldy <georgefeldy@gmail.com>
2025-09-20 15:00:12 +0300

    backcompat loading fix

--- Common/Systems/Power/MachineTE.cs
+++ Common/Systems/Power/MachineTE.cs
@@ -215,8 +215,8 @@ public abstract partial class MachineTE : ModTileEntity, IInventoryOwner
         ManuallyTurnedOff = tag.ContainsKey(nameof(ManuallyTurnedOff));
         Inventory = tag.TryGet(nameof(Inventory), out Inventory inventory) ? inventory : new(InventorySize, this);
         Inventory.Owner = this;
-        if(inventory.Size != InventorySize)
-            inventory.Size = InventorySize;
+        if(Inventory.Size != InventorySize)
+            Inventory.Size = InventorySize;
 
         MachineLoadData(tag);
     }
