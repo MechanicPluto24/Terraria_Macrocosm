0ff2d67cf04c7a910269c082b7c4621c9f0e7043
Yttrium125 <aktheawesome12@gmail.com>
2026-06-18 22:45:03 -0700

    really bad zombie botanist

---
+++ Content/Projectiles/Hostile/ZombieBotanistVine.cs
@@ -0,0 +1,46 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+using Microsoft.Xna.Framework;
+using Macrocosm.Content.NPCs.Enemies.Moon;
+using Terraria.Audio;
+using System.Collections;
+using Macrocosm.Common.DataStructures;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Macrocosm.Content.Dusts;
+
+namespace Macrocosm.Content.Projectiles.Hostile;
+public class ZombieBotanistVineBase : ModProjectile
+{
+    public override string Texture => "Macrocosm/Content/Projectiles/Hostile/ZombieBotanistVineBase";
+
+    public override void SetDefaults()
+    {
+        Projectile.width = 15;
+        Projectile.height = 16;
+        Projectile.hostile = true;
+        Projectile.friendly = false;
+        Projectile.timeLeft = 1200;
+        Projectile.tileCollide = false;
+        Projectile.aiStyle = ProjAIStyleID.Arrow;
+    }
+}
+
+public class ZombieBotanistVineTip : ModProjectile
+{
+    public override string Texture => "Macrocosm/Content/Projectiles/Hostile/ZombieBotanistVineTip";
+
+    public override void SetDefaults()
+    {
+        Projectile.width = 15;
+        Projectile.height = 16;
+        Projectile.hostile = true;
+        Projectile.friendly = false;
+        Projectile.timeLeft = 1200;
+        Projectile.tileCollide = true;
+        Projectile.aiStyle = ProjAIStyleID.Vilethorn;
+        AIType = ProjectileID.VilethornTip;
+    }
+}
